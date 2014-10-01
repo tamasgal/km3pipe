fb893ea3db559bb23b6e35f42ebcc4da85cacead
tamasgal <himself@tamasgal.com>
2014-10-01 17:12:54 +0200

    New pump

--- example.py
+++ example.py
@@ -5,6 +5,19 @@ __author__ = 'tamasgal'
 from km3pipe.core import Pipeline, Module
 
 
+class Pump(Module):
+
+    def __init__(self, **context):
+        super(self.__class__, self).__init__(**context)
+        self.blobs = self.get('blobs') or [{'foo': 1}, {'bar': 2}]
+
+    def process(self, blob):
+        try:
+            return self.blobs.pop(0)
+        except IndexError:
+            raise StopIteration
+
+
 class Foo(Module):
 
     def __init__(self, **context):
@@ -13,22 +26,21 @@ class Foo(Module):
         self.bar = self.get('bar') or 23
 
     def process(self, blob):
-        print("Processing " + str(self.name))
         print("This is the current blob: " + str(blob))
         print("foo={0}".format(self.foo))
         print("bar={0}".format(self.bar))
-        blob['foo_entry'] = self.foo
+        blob['foo'] = self.foo
         return blob
 
 class Moo(Module):
     def process(self, blob):
-        print("Processing " + self.name)
-        blob['moo_entry'] = 42
+        #blob['moo_entry'] = 42
         print("This is the blob after processing Moo: " + str(blob))
         return blob
 
 
 pipe = Pipeline()
-pipe.attach(Foo, 'foo_module')
+pipe.attach(Pump, 'the_pump')
+pipe.attach(Foo, 'foo_module', foo='dummyfoo', bar='dummybar')
 pipe.attach(Moo, 'moo_module')
 pipe.drain()
