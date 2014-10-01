20a436545911c78a569df063457cbbb35a1cb45c
tamasgal <himself@tamasgal.com>
2014-10-01 23:49:32 +0200

    Updates example with the new pump procedure

--- example.py
+++ example.py
@@ -9,14 +9,17 @@ class Pump(Module):
 
     def __init__(self, **context):
         super(self.__class__, self).__init__(**context)
-        self.blobs = self.get('blobs') or [{'foo': 1}, {'bar': 2}]
+        self.data = [{'nr': 1}, {'nr': 2}]
+        self.blobs = self.blob_generator()
 
     def process(self, blob):
-        try:
-            return self.blobs.pop(0)
-        except IndexError:
-            raise StopIteration
+        return next(self.blobs)
 
+    def blob_generator(self):
+        for blob in self.data:
+            print("yielding: " + str(blob))
+            yield blob
+        print("I'm done!")
 
 class Foo(Module):
 
@@ -27,20 +30,22 @@ class Foo(Module):
 
     def process(self, blob):
         print("This is the current blob: " + str(blob))
-        print("foo={0}".format(self.foo))
-        print("bar={0}".format(self.bar))
-        blob['foo'] = self.foo
+        blob['foo_entry'] = self.foo
         return blob
 
 class Moo(Module):
     def process(self, blob):
-        #blob['moo_entry'] = 42
-        print("This is the blob after processing Moo: " + str(blob))
+        blob['moo_entry'] = 42
         return blob
 
+class PrintBlob(Module):
+    def process(self, blob):
+        print(blob)
+        return blob
 
 pipe = Pipeline()
 pipe.attach(Pump, 'the_pump')
 pipe.attach(Foo, 'foo_module', foo='dummyfoo', bar='dummybar')
 pipe.attach(Moo, 'moo_module')
+pipe.attach(PrintBlob, 'print_blob')
 pipe.drain()
