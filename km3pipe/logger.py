b90842ef8557ff0b2f0171bc31f7cbfd66cf4e83
tamasgal <himself@tamasgal.com>
2014-10-04 20:09:21 +0200

    Refactorings

--- km3pipe/logger.py
+++ km3pipe/logger.py
@@ -2,10 +2,14 @@ __author__ = 'tamasgal'
 
 import logging
 
-logging.addLevelName(logging.INFO, "\033[1;32m%s\033[1;0m" % logging.getLevelName(logging.INFO))
-logging.addLevelName(logging.DEBUG, "\033[1;34m%s\033[1;0m" % logging.getLevelName(logging.DEBUG))
-logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" % logging.getLevelName(logging.WARNING))
-logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" % logging.getLevelName(logging.ERROR))
+logging.addLevelName(logging.INFO, "\033[1;32m%s\033[1;0m" %
+                     logging.getLevelName(logging.INFO))
+logging.addLevelName(logging.DEBUG, "\033[1;34m%s\033[1;0m" %
+                     logging.getLevelName(logging.DEBUG))
+logging.addLevelName(logging.WARNING, "\033[1;33m%s\033[1;0m" %
+                     logging.getLevelName(logging.WARNING))
+logging.addLevelName(logging.ERROR, "\033[1;31m%s\033[1;0m" %
+                     logging.getLevelName(logging.ERROR))
 
 formatter = logging.Formatter('[%(levelname)s] %(name)s: %(message)s')
 
