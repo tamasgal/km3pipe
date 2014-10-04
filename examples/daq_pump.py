a0c353aab16d8a2b265fc0e714f9b94be4bc415c
tamasgal <himself@tamasgal.com>
2014-10-04 19:12:16 +0200

    DAQ pump example

---
+++ examples/daq_pump.py
@@ -0,0 +1,11 @@
+__author__ = 'tamasgal'
+
+from km3pipe import Pipeline
+from km3pipe.pumps import DAQPump
+
+pipeline = Pipeline()
+pipeline.attach(DAQPump, 'daq_pump',
+                filename='/Users/tamasgal/Desktop/RUN-PPM_DU-00430-20140730-121124_detx.dat')
+pipeline.drain()
+
+
