34b150381e6bbbb772b8093527ca5d61111ad0d3
tamasgal <himself@tamasgal.com>
2014-10-06 23:08:28 +0200

    Constants and units

---
+++ km3pipe/constants.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+# Filename: constants.py
+"""
+The constants used in KM3Pipe.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+import math
+
+
+c = 2.99792458e8  # m/s
+
+n_water_antares_phase = 1.3499
+n_water_antares_group = 1.3797
+n_water_antares = n_water_antares_group
+theta_cherenkov_water_antares = math.acos(1 / n_water_antares_phase)
+c_water_antares = c / n_water_antares_group
+
+# Math
+pi = math.pi
+e = math.e
+
+# Default values for time residuals
+dt_window_l = -15  # ns
+dt_window_h = +25  # ns
---
+++ km3pipe/units.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+# Filename: units.py
+"""
+...
+
+"""
+from __future__ import division, absolute_import, print_function
