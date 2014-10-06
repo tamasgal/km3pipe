12d5bc1aed1ed7c66c59e869d4a24abad2fa204b
tamasgal <himself@tamasgal.com>
2014-10-06 15:24:01 +0200

    parse_doms warns if additional pmt params are found

--- km3pipe/hardware.py
+++ km3pipe/hardware.py
@@ -2,8 +2,10 @@ from __future__ import division, absolute_import, print_function
 
 __author__ = 'tamasgal'
 
-from km3pipe.tools import unpack_nfirst
+from km3pipe.tools import unpack_nfirst, split
+from km3pipe.logger import get_logger
 
+log = get_logger(__name__)
 
 class Detector(object):
     def __init__(self, filename=None):
@@ -23,7 +25,7 @@ class Detector(object):
         """Extract information from the header of the detector file"""
         self.det_file.seek(0, 0)
         first_line = self.det_file.readline()
-        self.det_id, self.n_doms = [int(i) for i in first_line.split()]
+        self.det_id, self.n_doms = split(first_line, int)
 
     def parse_doms(self):
         """Extract dom information from detector file"""
@@ -35,12 +37,14 @@ class Detector(object):
                 line = lines.pop(0)
                 if not line:
                     continue
-                dom_id, line_id, floor_id, n_pmts = [int(i) for i in line.split()]
+                dom_id, line_id, floor_id, n_pmts = split(line, int)
                 self.doms[dom_id] = (line_id, floor_id, n_pmts)
                 for i in range(n_pmts):
                     raw_pmt_info = lines.pop(0)
                     pmt_info = raw_pmt_info.split()
                     pmt_id, x, y, z, dx, dy, dz, t0, rest = unpack_nfirst(pmt_info, 8)
+                    if rest:
+                        log.warn("Unexpected PMT values: '{0}'".format(rest))
                     pmt_id = int(pmt_id)
                     pmt_pos = (float(x), float(y), float(z))
                     pmt_dir = (float(dx), float(dy), float(dz))
