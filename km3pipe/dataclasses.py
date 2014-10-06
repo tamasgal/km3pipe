Fixes direction normalising

km3pipe/dataclasses.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 # Filename: dataclasses.py
+# pylint: disable=W0232,C0103,C0111
 """
 ...
 
@@ -9,7 +10,7 @@ from __future__ import division, absolute_import, print_function
 import numpy as np
 
 
-class Position(np.ndarray):
+class Point(np.ndarray):
     """Represents a point in a 3D space"""
     def __new__(cls, input_array=(np.nan, np.nan, np.nan)):
         """Add x, y and z to the ndarray"""
@@ -39,3 +40,56 @@ class Position(np.ndarray):
     @z.setter
     def z(self, value):
         self[2] = value
+
+
+class Position(Point):
+    """Represents a point in a 3D space"""
+    pass
+
+
+class Direction(Point):
+    """Represents a direction in a 3D space
+
+    The direction vector always normalises itself when an attribute is changed.
+
+    """
+    def __new__(cls, input_array=(1, 0, 0)):
+        """Add x, y and z to the ndarray"""
+        normed_array = np.array(input_array) / np.linalg.norm(input_array)
+        obj = np.asarray(normed_array).view(cls)
+        return obj
+
+    def normalise(self):
+        normed_array = self / np.linalg.norm(self)
+        self[0] = normed_array[0]
+        self[1] = normed_array[1]
+        self[2] = normed_array[2]
+
+    @property
+    def x(self):
+        return self[0]
+
+    @x.setter
+    def x(self, value):
+        self[0] = value
+        self.normalise()
+
+    @property
+    def y(self):
+        return self[1]
+
+    @y.setter
+    def y(self, value):
+        self[1] = value
+        self.normalise()
+
+    @property
+    def z(self):
+        return self[2]
+
+    @z.setter
+    def z(self, value):
+        self[2] = value
+        self.normalise()
+
+

km3pipe/tests/test_dataclasses.py
@@ -6,8 +6,11 @@
 """
 from __future__ import division, absolute_import, print_function
 
+import numpy as np
+
 from km3pipe.testing import *
-from km3pipe.dataclasses import Position
+from km3pipe.dataclasses import Position, Direction
+
 
 class TestPosition(TestCase):
 
@@ -35,3 +38,30 @@ class TestPosition(TestCase):
         self.assertEqual(4, pos[1])
         self.assertEqual(3, pos.size)
         self.assertTupleEqual((3,), pos.shape)
+
+
+class TestDirection(TestCase):
+
+    def test_direction(self):
+        dir = Direction((1, 0, 0))
+        self.assertAlmostEqual(1, np.linalg.norm(dir))
+        self.assertEqual(1, dir.x)
+        self.assertEqual(0, dir.y)
+        self.assertEqual(0, dir.z)
+
+    def test_direction_normalises_on_init(self):
+        dir = Direction((1, 2, 3))
+        self.assertAlmostEqual(0.26726124, dir.x)
+        self.assertAlmostEqual(0.53452248, dir.y)
+        self.assertAlmostEqual(0.80178372, dir.z)
+
+    def test_direction_normalises_on_change_attribute(self):
+        dir = Direction((1, 2, 3))
+        self.assertAlmostEqual(1, np.linalg.norm(dir))
+        dir.x = 10
+        self.assertAlmostEqual(1, np.linalg.norm(dir))
+        dir.y = 20
+        self.assertAlmostEqual(1, np.linalg.norm(dir))
+        dir.z = 30
+        self.assertAlmostEqual(1, np.linalg.norm(dir))
+