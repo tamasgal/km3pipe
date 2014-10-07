933f4c078fa45ddb18475044aa1d3f39eb8ab57c
tamasgal <himself@tamasgal.com>
2014-10-07 11:21:59 +0200

    Refactors

--- km3pipe/dataclasses.py
+++ km3pipe/dataclasses.py
@@ -7,6 +7,8 @@
 """
 from __future__ import division, absolute_import, print_function
 
+__all__ = ('Point', 'Position', 'Direction')
+
 import numpy as np
 
 
@@ -59,7 +61,7 @@ class Direction(Point):
         obj = np.asarray(normed_array).view(cls)
         return obj
 
-    def normalise(self):
+    def _normalise(self):
         normed_array = self / np.linalg.norm(self)
         self[0] = normed_array[0]
         self[1] = normed_array[1]
@@ -72,7 +74,7 @@ class Direction(Point):
     @x.setter
     def x(self, value):
         self[0] = value
-        self.normalise()
+        self._normalise()
 
     @property
     def y(self):
@@ -81,7 +83,7 @@ class Direction(Point):
     @y.setter
     def y(self, value):
         self[1] = value
-        self.normalise()
+        self._normalise()
 
     @property
     def z(self):
@@ -90,6 +92,6 @@ class Direction(Point):
     @z.setter
     def z(self, value):
         self[2] = value
-        self.normalise()
+        self._normalise()
 
 
