9a2ec607b2112223cf65e94c841e22d19bf1ad17
Tamas Gal <tgal@km3net.de>
2014-12-04 23:52:45 +0100

    Adds skipIf decorator

--- km3pipe/testing/__init__.py
+++ km3pipe/testing/__init__.py
@@ -7,9 +7,9 @@ Common unit testing support for km3pipe.
 from __future__ import division, absolute_import, print_function
 
 try:
-    from unittest2 import TestCase
+    from unittest2 import TestCase, skipIf
 except ImportError:
-    from unittest import TestCase
+    from unittest import TestCase, skipIf
 
 try:
     from cStringIO import StringIO
