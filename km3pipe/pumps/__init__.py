18c8abdc6e2d91eae3aa9a9c7d0539f3154dd4ed
Tamas Gal <tgal@km3net.de>
2014-12-05 20:18:09 +0100

    Adds aanet to the pumps

--- km3pipe/pumps/__init__.py
+++ km3pipe/pumps/__init__.py
@@ -8,3 +8,4 @@ from __future__ import division, absolute_import, print_function
 
 from km3pipe.pumps.evt import EvtPump
 from km3pipe.pumps.daq import DAQPump
+from km3pipe.pumps.aanet import AanetPump
