ff1d69917c10fb74bf44535c120cb946d04d01db
Tamas Gal <tgal@km3net.de>
2014-12-11 19:35:20 +0100

    Adds env var for km3pipe location

--- setenv.sh
+++ setenv.sh
@@ -7,4 +7,6 @@ if ! ( echo ${ENV_DIR} | egrep ${PYTHONPATH} > /dev/null ); then
     export PYTHONPATH=${ENV_DIR}:${PYTHONPATH}
 fi
 
+export KM3PIPEDIR=${ENV_DIR}
+
 alias pipeinspector='python ${ENV_DIR}/pipeinspector/app.py'
