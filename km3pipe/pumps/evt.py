80110d7b0c865543ce1b5934bdd3e2e0c14e7002
Tamas Gal <himself@tamasgal.com>
2015-01-14 14:46:28 +0100

    Implements process for the EvtPump

--- km3pipe/pumps/evt.py
+++ km3pipe/pumps/evt.py
@@ -22,9 +22,9 @@ class EvtPump(Pump):
         super(self.__class__, self).__init__(**context)
         self.filename = self.get('filename')
 
-        self.blobs = None
         self.raw_header = None
         self.event_offsets = []
+        self.index = 0
 
         if self.filename:
             self.open_file(self.filename)
@@ -36,7 +36,6 @@ class EvtPump(Pump):
         """Populate the blobs"""
         self.raw_header = self.extract_header()
         self._rebuild_offsets()
-        self.blobs = self.blob_generator()
 
     def extract_header(self):
         """Create a dictionary with the EVT header information"""
@@ -53,6 +52,18 @@ class EvtPump(Pump):
                 return raw_header
         raise ValueError("Incomplete header, no 'end_event' tag found!")
 
+    def get_blob(self, index):
+        """Return a blob with the event at the given index"""
+        self.blob_file.seek(self.event_offsets[index], 0)
+        blob = self._create_blob()
+        return blob
+
+    def process(self, blob):
+        """Pump the next blob to the modules"""
+        blob = self.get_blob(self.index)
+        self.index += 1
+        return blob
+
     def _rebuild_offsets(self):
         self.blob_file.seek(0, 0)
         self.event_offsets = []
@@ -61,17 +72,12 @@ class EvtPump(Pump):
             if line.startswith('end_event:'):
                 self._record_offset()
         self.event_offsets.pop()  # get rid of the last one
+        self.blob_file.seek(self.event_offsets[0], 0)
 
     def _record_offset(self):
         """Stores the current file pointer position"""
         offset = self.blob_file.tell()
         self.event_offsets.append(offset)
-
-    def get_blob(self, index):
-        """Return a blob with the event at the given index"""
-        self.blob_file.seek(self.event_offsets[index], 0)
-        blob = self._create_blob()
-        return blob
 
     def _create_blob(self):
         blob = None
@@ -92,33 +98,6 @@ class EvtPump(Pump):
                     blob.setdefault(tag, []).append(values)
                 else:
                     blob[tag] = value.split()
-
-    def blob_generator(self):
-        """Create a generator object which extracts events from an EVT file."""
-        blob = None
-        for line in self.blob_file:
-            line = line.strip()
-            if line.startswith('end_event:') and blob:
-                blob['raw_header'] = self.raw_header
-                yield blob
-                blob = None
-                continue
-            if line.startswith('start_event:'):
-                blob = {}
-                tag, value = line.split(':')
-                blob[tag] = value.split()
-                continue
-            if blob:
-                tag, value = line.split(':')
-                if tag in ('neutrino', 'track_in', 'hit'):
-                    values = [float(x) for x in value.split()]
-                    blob.setdefault(tag, []).append(values)
-                else:
-                    blob[tag] = value.split()
-
-    def process(self, blob):
-        """Pump the next blob to the modules"""
-        return next(self.blobs)
 
     def finish(self):
         """Clean everything up"""
