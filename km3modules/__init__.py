c29c629ba75bf4109262960f437b075d113c0f25
Tamas Gal <himself@tamasgal.com>
2015-01-15 11:47:41 +0100

    StatusBar module with time measurement

--- km3modules/__init__.py
+++ km3modules/__init__.py
@@ -1,21 +1,41 @@
+# coding=utf-8
+# Filename: __init__.py
+# pylint: disable=locally-disabled
+"""
+A collection of commonly used modules.
+
+"""
+from __future__ import division, absolute_import, print_function
+
+import datetime
+
 from km3pipe import Module
 
 
 class HitCounter(Module):
+    """Prints the number of hits and raw hits in an Evt file"""
     def process(self, blob):
-        print("Number of hits: " + str(len(blob['hits'])))
+        print("Number of hits: {0}".format(len(blob['hit'])))
+        print("Number of raw hits: {0}".format(len(blob['hit_raw'])))
         return blob
 
-class FirstHit(Module):
+
+class StatusBar(Module):
+    """Displays the current blob number"""
+    def __init__(self, **context):
+        super(self.__class__, self).__init__(**context)
+        self.blob_index = 0
+
+
+        self.start = datetime.datetime.now()
+
     def process(self, blob):
-        hits = blob['hits']
-        print("First hit: " + str(hits[0]))
-        print("Second hit: " + str(hits[1]))
+        print("----[Blob {0:>5}]----".format(self.blob_index))
+        self.blob_index += 1
         return blob
 
-class PrintTrks(Module):
-    def process(self, blob):
-        trks = blob['mc_trks']
-        for trk in trks:
-            print(trk)
-        return blob
+    def finish(self):
+        elapsed_time = self.start - datetime.datetime.now()
+        print("\n" + '='*42)
+        print("Processed {0} blobs in {1} s."
+              .format(self.blob_index, elapsed_time.microseconds / 1e6))
