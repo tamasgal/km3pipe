22c95f85b40da4e7d31436e5667e666f5d806d62
Tamas Gal <himself@tamasgal.com>
2015-01-15 16:53:09 +0100

    Adds namedtuple_with_defaults factory

--- km3pipe/tools.py
+++ km3pipe/tools.py
@@ -9,6 +9,7 @@ from __future__ import division, absolute_import, print_function
 
 __author__ = 'tamasgal'
 
+from collections import namedtuple
 
 def unpack_nfirst(seq, nfirst):
     """Unpack the nfrist items from the list and return the rest.
@@ -40,3 +41,26 @@ def split(string, callback=None):
     else:
         return string.split()
 
+def namedtuple_with_defaults(typename, field_names, default_values=[]):
+    """Create a namedtuple with default values
+
+    >>> Node = namedtuple_with_defaults('Node', 'val left right')
+    >>> Node()
+    Node(val=None, left=None, right=None)
+    >>> Node = namedtuple_with_defaults('Node', 'val left right', [1, 2, 3])
+    >>> Node()
+    Node(val=1, left=2, right=3)
+    >>> Node = namedtuple_with_defaults('Node', 'val left right', {'right':7})
+    >>> Node()
+    Node(val=None, left=None, right=7)
+    >>> Node(4)
+    Node(val=4, left=None, right=7)
+    """
+    T = namedtuple(typename, field_names)
+    T.__new__.__defaults__ = (None,) * len(T._fields)
+    if isinstance(default_values, collections.Mapping):
+        prototype = T(**default_values)
+    else:
+        prototype = T(*default_values)
+    T.__new__.__defaults__ = tuple(prototype)
+    return T
