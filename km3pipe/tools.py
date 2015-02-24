f8d4c231a24cddb9a481e701661f6840e6e3c5b8
Tamas Gal <himself@tamasgal.com>
2015-02-24 17:37:09 +0100

    Adds some vector alg

--- km3pipe/tools.py
+++ km3pipe/tools.py
@@ -11,6 +11,9 @@ __author__ = 'tamasgal'
 
 from collections import namedtuple
 
+import numpy as np
+
+
 def unpack_nfirst(seq, nfirst):
     """Unpack the nfrist items from the list and return the rest.
 
@@ -64,6 +67,32 @@ def namedtuple_with_defaults(typename, field_names, default_values=[]):
         prototype = T(*default_values)
     T.__new__.__defaults__ = tuple(prototype)
     return T
+
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    angle = np.arccos(np.dot(v1_u, v2_u))
+    if np.isnan(angle):
+        if (v1_u == v2_u).all():
+            return 0.0
+        else:
+            return np.pi
+    return angle
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
 
 def geant2pdg(geant_code):
     """Convert GEANT particle ID to PDG"""
