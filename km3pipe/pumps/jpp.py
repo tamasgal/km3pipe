5f6550651228cbbe87e9f447ed92c163a7779b9b
Tamas Gal <himself@tamasgal.com>
2015-04-02 20:24:41 +0200

    Moves aa and root imports to init

--- km3pipe/pumps/jpp.py
+++ km3pipe/pumps/jpp.py
@@ -6,17 +6,18 @@ from __future__ import division, absolute_import, print_function
 
 from km3pipe import Pump, Blob
 from km3pipe.logger import logging
-import aa
-import ROOT
+
 log = logging.getLogger(__name__)  # pylint: disable=C0103
 
 
 class JPPPump(Pump):
-    """A pump for binary jpp files."""
+    """A pump for JPP ROOT files."""
 
     def __init__(self, **context):
         super(self.__class__, self).__init__(**context)
 
+        import aa
+        import ROOT
 
         self.filename = self.get('filename')
         if not self.filename:
