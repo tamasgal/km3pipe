583ac575978716b5cc6730efa999a5ff694cde34
Tamas Gal <himself@tamasgal.com>
2015-04-16 23:32:44 +0200

    Moves EVT related classes from dataclasses.py to evt.py

--- km3pipe/dataclasses.py
+++ km3pipe/dataclasses.py
@@ -7,13 +7,12 @@
 """
 from __future__ import division, absolute_import, print_function
 
-__all__ = ('Point', 'Position', 'Direction', 'Hit')
+__all__ = ('Point', 'Position', 'Direction')
 
-from collections import namedtuple
 
 import numpy as np
 
-from km3pipe.tools import pdg2name, geant2pdg, angle_between
+from km3pipe.tools import angle_between
 
 class Point(np.ndarray):
     """Represents a point in a 3D space"""
@@ -105,105 +104,3 @@ class Direction(Point):
         return "({0:.4}, {1:.4}, {2:.4})".format(self.x, self.y, self.z)
 
 
-class Track(object):
-    """Bass class for particle or shower tracks"""
-    def __init__(self, id, x, y, z, dx, dy, dz, E=None, t=0, *args):
-        self.id = int(id)
-        # z correctio due to gen/km3 (ZED -> sea level shift)
-        # http://wiki.km3net.physik.uni-erlangen.de/index.php/Simulations
-        self.pos = Point((x, y, z + 405.93))
-        self.dir = Direction((dx, dy, dz))
-        self.E = E
-        self.time = t
-        self.args = args
-
-    def __repr__(self):
-        text = "Track:\n"
-        text += " id: {0}\n".format(self.id)
-        text += " pos: {0}\n".format(self.pos)
-        text += " dir: {0}\n".format(self.dir)
-        text += " energy: {0} GeV\n".format(self.E)
-        text += " time: {0} ns\n".format(self.time)
-        return text
-
-
-class TrackIn(Track):
-    """Representation of a track_in entry in an EVT file"""
-    def __init__(self, *context):
-        super(self.__class__, self).__init__(*context)
-        self.particle_type = geant2pdg(int(self.args[0]))
-        try:
-            self.length = self.args[1]
-        except IndexError:
-            self.length = 0
-
-    def __repr__(self):
-        text = super(self.__class__, self).__repr__()
-        text += " type: {0} '{1}' [PDG]\n".format(self.particle_type,
-                                                  pdg2name(self.particle_type))
-        text += " length: {0} [m]\n".format(self.length)
-        return text
-
-
-class TrackFit(Track):
-    """Representation of a track_fit entry in an EVT file"""
-    def __init__(self, *context):
-        super(self.__class__, self).__init__(*context)
-        self.speed = self.args[0]
-        self.ts = self.args[1]
-        self.te = self.args[2]
-        self.con1 = self.args[3]
-        self.con2 = self.args[4]
-
-    def __repr__(self):
-        text = super(self.__class__, self).__repr__()
-        text += " speed: {0} [m/ns]\n".format(self.speed)
-        text += " ts: {0} [ns]\n".format(self.ts)
-        text += " te: {0} [ns]\n".format(self.te)
-        text += " con1: {0}\n".format(self.con1)
-        text += " con2: {0}\n".format(self.con2)
-        return text
-
-
-class Neutrino(object):
-    """Representation of a neutrino entry in an EVT file"""
-    def __init__(self, id, x, y, z, dx, dy, dz, E, t, Bx, By,
-                 ichan, particle_type, channel, *args):
-        self.id = id
-        # z correctio due to gen/km3 (ZED -> sea level shift)
-        # http://wiki.km3net.physik.uni-erlangen.de/index.php/Simulations
-        self.pos = Point((x, y, z + 405.93))
-        self.dir = Direction((dx, dy, dz))
-        self.E = E
-        self.time = t
-        self.Bx = Bx
-        self.By = By
-        self.ichan = ichan
-        self.particle_type = particle_type
-        self.channel = channel
-
-    def __str__(self):
-        text = "Neutrino: "
-        text += pdg2name(self.particle_type)
-        if self.E >= 1000000:
-            text += ", {0:.3} PeV".format(self.E / 1000000)
-        elif self.E >= 1000:
-            text += ", {0:.3} TeV".format(self.E / 1000)
-        else:
-            text += ", {0:.3} GeV".format(float(self.E))
-        text += ', CC' if int(self.channel) == 2 else ', NC'
-        return text
-
-# The hit entry in an EVT file
-Hit = namedtuple('Hit', 'id pmt_id pe time type n_photons track_in c_time')
-Hit.__new__.__defaults__ = (None, None, None, None, None, None, None, None)
-
-# The hit_raw entry in an EVT file
-def __add_raw_hit__(self, other):
-    """Add two hits by adding the ToT and preserve time and pmt_id
-    of the earlier one."""
-    first = self if self.time <= other.time else other
-    return RawHit(first.id, first.pmt_id, self.tot+other.tot, first.time)
-RawHit = namedtuple('RawHit', 'id pmt_id tot time')
-RawHit.__new__.__defaults__ = (None, None, None, None)
-RawHit.__add__ = __add_raw_hit__
--- km3pipe/pumps/evt.py
+++ km3pipe/pumps/evt.py
@@ -11,9 +11,13 @@ __author__ = 'tamasgal'
 
 import sys
 
+from collections import namedtuple
+
 from km3pipe import Pump
-from km3pipe.dataclasses import Hit, RawHit, TrackIn, TrackFit, Neutrino
 from km3pipe.logger import logging
+
+from km3pipe.dataclasses import Point, Direction, Position
+from km3pipe.tools import pdg2name, geant2pdg, unpack_nfirst
 
 log = logging.getLogger(__name__)  # pylint: disable=C0103
 
@@ -156,13 +160,13 @@ class EvtPump(Pump):
                     if tag == "hit_raw":
                         blob.setdefault("EvtRawHits", []).append(RawHit(*values))
                     if tag == "track_in":
-                        blob.setdefault("TrackIns", []).append(TrackIn(*values))
+                        blob.setdefault("TrackIns", []).append(TrackIn(values))
                     if tag == "track_fit":
-                        blob.setdefault("TrackFits", []).append(TrackFit(*values))
+                        blob.setdefault("TrackFits", []).append(TrackFit(values))
                 else:
                     if tag == 'neutrino':
                         values = [float(x) for x in value.split()]
-                        blob['Neutrino'] = Neutrino(*values)
+                        blob['Neutrino'] = Neutrino(values)
                     else:
                         blob[tag] = value.split()
 
@@ -185,3 +189,112 @@ class EvtPump(Pump):
     def finish(self):
         """Clean everything up"""
         self.blob_file.close()
+
+
+class Track(object):
+    """Bass class for particle or shower tracks"""
+#    def __init__(self, id, x, y, z, dx, dy, dz, E=None, t=0, *args):
+    def __init__(self, data, zed_correction=405.93):
+        id, x, y, z, dx, dy, dz, E, t, args = unpack_nfirst(data, 9)
+        self.id = int(id)
+        # z correctio due to gen/km3 (ZED -> sea level shift)
+        # http://wiki.km3net.physik.uni-erlangen.de/index.php/Simulations
+        self.pos = Point((x, y, z + zed_correction))
+        self.dir = Direction((dx, dy, dz))
+        self.E = E
+        self.time = t
+        self.args = args
+
+    def __repr__(self):
+        text = "Track:\n"
+        text += " id: {0}\n".format(self.id)
+        text += " pos: {0}\n".format(self.pos)
+        text += " dir: {0}\n".format(self.dir)
+        text += " energy: {0} GeV\n".format(self.E)
+        text += " time: {0} ns\n".format(self.time)
+        return text
+
+
+class TrackIn(Track):
+    """Representation of a track_in entry in an EVT file"""
+    def __init__(self, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
+        self.particle_type = geant2pdg(int(self.args[0]))
+        try:
+            self.length = self.args[1]
+        except IndexError:
+            self.length = 0
+
+    def __repr__(self):
+        text = super(self.__class__, self).__repr__()
+        text += " type: {0} '{1}' [PDG]\n".format(self.particle_type,
+                                                  pdg2name(self.particle_type))
+        text += " length: {0} [m]\n".format(self.length)
+        return text
+
+
+class TrackFit(Track):
+    """Representation of a track_fit entry in an EVT file"""
+    def __init__(self, *args, **kwargs):
+        super(self.__class__, self).__init__(*args, **kwargs)
+        self.speed = self.args[0]
+        self.ts = self.args[1]
+        self.te = self.args[2]
+        self.con1 = self.args[3]
+        self.con2 = self.args[4]
+
+    def __repr__(self):
+        text = super(self.__class__, self).__repr__()
+        text += " speed: {0} [m/ns]\n".format(self.speed)
+        text += " ts: {0} [ns]\n".format(self.ts)
+        text += " te: {0} [ns]\n".format(self.te)
+        text += " con1: {0}\n".format(self.con1)
+        text += " con2: {0}\n".format(self.con2)
+        return text
+
+
+class Neutrino(object):
+    """Representation of a neutrino entry in an EVT file"""
+    #def __init__(self, id, x, y, z, dx, dy, dz, E, t, Bx, By,
+    #             ichan, particle_type, channel, *args):
+    def __init__(self, data, zed_correction=405.93):
+        id, x, y, z, dx, dy, dz, E, t, Bx, By, \
+            ichan, particle_type, channel, args = unpack_nfirst(data, 14)
+        self.id = id
+        # z correctio due to gen/km3 (ZED -> sea level shift)
+        # http://wiki.km3net.physik.uni-erlangen.de/index.php/Simulations
+        self.pos = Point((x, y, z + zed_correction))
+        self.dir = Direction((dx, dy, dz))
+        self.E = E
+        self.time = t
+        self.Bx = Bx
+        self.By = By
+        self.ichan = ichan
+        self.particle_type = particle_type
+        self.channel = channel
+
+    def __str__(self):
+        text = "Neutrino: "
+        text += pdg2name(self.particle_type)
+        if self.E >= 1000000:
+            text += ", {0:.3} PeV".format(self.E / 1000000)
+        elif self.E >= 1000:
+            text += ", {0:.3} TeV".format(self.E / 1000)
+        else:
+            text += ", {0:.3} GeV".format(float(self.E))
+        text += ', CC' if int(self.channel) == 2 else ', NC'
+        return text
+
+# The hit entry in an EVT file
+Hit = namedtuple('Hit', 'id pmt_id pe time type n_photons track_in c_time')
+Hit.__new__.__defaults__ = (None, None, None, None, None, None, None, None)
+
+# The hit_raw entry in an EVT file
+def __add_raw_hit__(self, other):
+    """Add two hits by adding the ToT and preserve time and pmt_id
+    of the earlier one."""
+    first = self if self.time <= other.time else other
+    return RawHit(first.id, first.pmt_id, self.tot+other.tot, first.time)
+RawHit = namedtuple('RawHit', 'id pmt_id tot time')
+RawHit.__new__.__defaults__ = (None, None, None, None)
+RawHit.__add__ = __add_raw_hit__
