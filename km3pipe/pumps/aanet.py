c6f65e704c6ee423a9060bd894bae3b3b3d60844
Tamas Gal <himself@tamasgal.com>
2015-04-23 19:18:06 +0200

    Fixes some pyling violations

--- km3pipe/pumps/aanet.py
+++ km3pipe/pumps/aanet.py
@@ -7,7 +7,7 @@ Pump for the Aanet data format.
 """
 from __future__ import division, absolute_import, print_function
 
-from km3pipe import Pump, Blob
+from km3pipe import Pump
 from km3pipe.logger import logging
 
 log = logging.getLogger(__name__)  # pylint: disable=C0103
@@ -18,8 +18,10 @@ class AanetPump(Pump):
 
     def __init__(self, **context):
         super(self.__class__, self).__init__(**context)
-        import aa
-        from ROOT import TFile, Evt, Trk
+        
+        import aa # pylint: disable:F0401,W0612
+        from ROOT import TFile, Evt, Trk # pylint: disable:F0401,W0612
+        
         self.filename = self.get('filename')
         if not self.filename:
             raise ValueError("No filename defined")
