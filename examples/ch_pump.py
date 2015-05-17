13566e855fc33a0e9562c051ab37476437ba72a9
Tamas Gal <himself@tamasgal.com>
2015-05-18 01:37:27 +0200

    Updates controlhost example

--- examples/ch_pump.py
+++ examples/ch_pump.py
@@ -3,19 +3,14 @@ import time
 from km3pipe import Pipeline, Module
 from km3pipe.pumps import CHPump
 
-class Sleep(Module):
+class CHPrinter(Module):
     def process(self, blob):
-        time.sleep(5)
-        return blob
-
-class PrintBlob(Module):
-    def process(self, blob):
-        print blob
+        print blob['CHPrefix']
+        print blob['CHData']
         return blob
 
 pipe = Pipeline()
-pipe.attach(CHPump, host='127.0.0.1')
-pipe.attach(PrintBlob)
-pipe.attach(Sleep)
+pipe.attach(CHPump, host='127.0.0.1', port=5553, tag='foo')
+pipe.attach(CHPrinter)
 pipe.drain(10)
 
