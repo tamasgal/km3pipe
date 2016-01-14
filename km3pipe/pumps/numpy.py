4bc03057afa263a2f054c7de19676db88eabf3e7
Moritz Lotze <lotzemoritz@gmail.com>
2016-01-14 19:14:13 +0100

    limit to only structured arrays

--- km3pipe/pumps/numpy.py
+++ km3pipe/pumps/numpy.py
@@ -2,33 +2,32 @@
 # Filename: numpy.py
 # pylint: disable=locally-disabled
 # vim:set ts=4 sts=4 sw=4 et:
-""" Pump for Numpy arrays.
+"""Streamer for Numpy arrays.
 """
 from __future__ import division, absolute_import, print_function
 
 import numpy as np
 
-from km3pipe.core import Pump
+from km3pipe.core import Pump, Blob
 
 
-class NumpyPump(Pump):
-    """A pump for Numpy structured arrays.
+class NumpyStructuredPump(Pump):
+    """NumpyStructuredPump streams events from structured Numpy arrays.
     """
-    def __init__(self, columns=None, **context):
+
+    def __init__(self, **context):
         super(self.__class__, self).__init__(**context)
 
-        self.filename = self.get("filename")
+        self.array = self.get("array")
         columns = self.get("columns")
 
-        with np.load(self.filename) as array:
-            self.array = array
         if columns:
             self.columns = columns
         else:
             self.columns = self.array.dtype.names
 
-    def blob(self, index):
-        blob = {}
+    def get_blob(self, index):
+        blob = Blob()
         sample = self.array[index]
         for key in self.columns:
             blob[key] = sample[key][index]
