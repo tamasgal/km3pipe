d58e73e3c7f37036b2c71271ee4c493b76e1b4ee
Tamas Gal <himself@tamasgal.com>
2016-01-19 15:19:54 +0100

    Prompts user for input if password is not in config

--- km3pipe/config.py
+++ km3pipe/config.py
@@ -10,6 +10,7 @@ from __future__ import division, absolute_import, print_function
 __author__ = 'tamasgal'
 
 import ConfigParser, os
+import getpass
 
 import logging
 from km3pipe.logger import logging
@@ -24,12 +25,19 @@ class Config(object):
     def __init__(self):
         """Configuration manager for KM3NeT stuff"""
         self.config = ConfigParser.ConfigParser()
-        self.config.readfp(open(os.path.expanduser(CONFIG_PATH)))
+        try:
+            self.config.readfp(open(os.path.expanduser(CONFIG_PATH)))
+        except IOError:
+            log.error("No configuration found at '{0}'".format(CONFIG_PATH))
 
     @property
     def db_credentials(self):
         """Return username and password for the KM3NeT WebDB."""
-        username = self.config.get('DB', 'username')
-        password = self.config.get('DB', 'password')
+        try:
+            username = self.config.get('DB', 'username')
+            password = self.config.get('DB', 'password')
+        except ConfigParser.NoSectionError:
+            username = raw_input("Please enter your KM3NeT DB username: ")
+            password = getpass.getpass("Password: ")
         return username, password
 
