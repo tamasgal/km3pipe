2cfd0b2a93c345400b1f8c03c56632c17dfe3781
Tamas Gal <himself@tamasgal.com>
2016-01-31 23:24:36 +0100

    Unit tests for DOMContainer

--- km3pipe/tests/test_db.py
+++ km3pipe/tests/test_db.py
@@ -10,6 +10,13 @@ from km3pipe.db import DOMContainer
 __author__ = 'tamasgal'
 
 
+DET_ID = 'det_id1'
+JSON_DOMS = [{'DOMId': 1, 'Floor': 10, 'CLBUPI': '100', 'DetOID': DET_ID},
+             {'DOMId': 2, 'Floor': 20, 'CLBUPI': '200', 'DetOID': DET_ID},
+             {'DOMId': 3, 'Floor': 30, 'CLBUPI': '300', 'DetOID': DET_ID},
+             {'DOMId': 4, 'Floor': 40, 'CLBUPI': '400', 'DetOID': 'det_id2'}]
+
+
 class TestDBManager(TestCase):
     pass
 
@@ -18,11 +25,22 @@ class TestDOMContainer(TestCase):
     def test_init(self):
         DOMContainer(None)
 
+    def setUp(self):
+        self.dc = DOMContainer(JSON_DOMS)
+
     def test_ids_returns_dom_ids(self):
-        det_id = 'a'
-        json_data = [{'DOMId': 1, 'DetOID': det_id},
-                     {'DOMId': 2, 'DetOID': det_id},
-                     {'DOMId': 3, 'DetOID': det_id},
-                     {'DOMId': 4, 'DetOID': 'another_det_id'}]
-        dc = DOMContainer(json_data)
-        self.assertListEqual([1, 2, 3], dc.ids(det_id))
+        self.assertListEqual([1, 2, 3], self.dc.ids(DET_ID))
+
+    def test_json_list_lookup(self):
+        lookup = self.dc._json_list_lookup('DOMId', 1, 'Floor', DET_ID)
+        self.assertEqual(10, lookup)
+
+    def test_clbupi2floor(self):
+        self.assertEqual(10, self.dc.clbupi2floor('100', DET_ID))
+        self.assertEqual(20, self.dc.clbupi2floor('200', DET_ID))
+        self.assertEqual(30, self.dc.clbupi2floor('300', DET_ID))
+
+    def test_clbupi2domid(self):
+        self.assertEqual(1, self.dc.clbupi2domid('100', DET_ID))
+        self.assertEqual(2, self.dc.clbupi2domid('200', DET_ID))
+        self.assertEqual(3, self.dc.clbupi2domid('300', DET_ID))
