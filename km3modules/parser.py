d508836694368fb99280c44516178df8d25c97fd
Tamas Gal <himself@tamasgal.com>
2016-03-11 01:31:43 +0100

    Implements controlhost parser which chooses the right class when dealing with binary data.

--- km3modules/parser.py
+++ km3modules/parser.py
@@ -8,18 +8,22 @@ A collection of parsers.
 from __future__ import division, absolute_import, print_function
 
 from km3pipe import Module
-from km3pipe.pumps.daq import DAQPreamble, DAQSummaryslice
-
-try:
-    from cStringIO import StringIO
-except ImportError:
-    try:
-        from StringIO import StringIO
-    except ImportError:
-        from io import StringIO
+from km3pipe.pumps.daq import DAQPreamble, DAQSummaryslice, DAQEvent
+from km3pipe.common import StringIO
 
 
 class CHParser(Module):
+    """A parser for ControlHost data.
+
+    This parser will choose the correct class to parse the binary data
+    for given `tags`.
+
+    """
+    parse_map = {
+        'IO_SUM': ['DAQSummaryslice', DAQSummaryslice],
+        'IO_EVT': ['DAQEvent', DAQEvent],
+        }
+
     def __init__(self, **context):
         super(self.__class__, self).__init__(**context)
         self.tags = self.get('tags') or []
@@ -30,14 +34,14 @@ class CHParser(Module):
 
         tag = str(blob['CHPrefix'].tag)
 
-        if tag not in self.tags:
+        if tag not in self.tags and tag not in self.parse_map:
             return blob
 
-        if tag == 'IO_SUM':
-            data = blob['CHData']
-            data_io = StringIO(data)
-            preamble = DAQPreamble(file_obj=data_io)  # noqa
-            summaryslice = DAQSummaryslice(file_obj=data_io)
-            blob['DAQSummaryslice'] = summaryslice
+        data = blob['CHData']
+        data_io = StringIO(data)
+        preamble = DAQPreamble(file_obj=data_io)  # noqa
+
+        blob_key, ParserClass = self.parse_map[tag]
+        blob[blob_key] = ParserClass(file_obj=data_io)
 
         return blob
