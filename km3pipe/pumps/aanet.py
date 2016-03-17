58f76fd9a921ab67b1a40c3b804de6b3eb0ed7c8
heidtgg <heid.tgg@googlemail.com>
2016-03-17 07:37:04 +0100

    decide for which id to check, set  standard to frame_index

--- km3pipe/pumps/aanet.py
+++ km3pipe/pumps/aanet.py
@@ -24,6 +24,7 @@ class AanetPump(Pump):
         self.filenames = self.get('filenames') or []
         self.indices = self.get('indices')
         self.additional = self.get('additional')
+        self.id = self.get('id')
 
         if not self.filename and not self.filenames:
             raise ValueError("No filename(s) defined")
@@ -87,18 +88,24 @@ class AanetPump(Pump):
                 yield blob
             del event_file
 
+    def event_index(self, blob):
+        if self.id:
+            return blob["Evt"].id
+        else:
+            return blob["Evt"].frame_index
+
     def process(self, blob=None):
         if self.additional:
             new_blob = self.previous
-            if new_blob["Evt"].frame_index == blob["Evt"].frame_index:
+            if self.event_index(new_blob) == blob["Evt"].frame_index:
                 blob[self.additional] = new_blob
                 return blob
 
-            elif new_blob["Evt"].frame_index > blob["Evt"].frame_index:
+            elif self.event_index(new_blob) > blob["Evt"].frame_index:
                 return None
 
             else:
-                while new_blob["Evt"].frame_index < blob["Evt"].frame_index:
+                while self.event_index(new_blob) < blob["Evt"].frame_index:
                     new_blob = next(self.blobs)
 
                 self.previous = new_blob
