4ebc6f5bae09c0dc99bd65ee025faec58294eea2
Tamas Gal <himself@tamasgal.com>
2016-04-20 01:18:25 +0200

    Ignore ImportError since it will interrupt pip install

--- km3pipe/__init__.py
+++ km3pipe/__init__.py
@@ -6,17 +6,19 @@ The extemporary KM3NeT analysis framework.
 """
 from __future__ import division, absolute_import, print_function
 
-import pyximport
-pyximport.install()
 
 from km3pipe.__version__ import version, version_info  # noqa
 
 try:
+    import pyximport
+    pyximport.install()
+
     from km3pipe.core import (Pipeline, Module, Pump, Blob,  # noqa
                               Geometry, AanetGeometry)
     from km3pipe import pumps  # noqa
 except ImportError:
-    print("Numpy is needed for KM3Pipe")
+    print("Some modules could not be imported. Ignore this if you're "
+          "installing or upgrading KM3Pipe.")
 
 __author__ = "Tamas Gal"
 __copyright__ = ("Copyright 2015, Tamas Gal and the KM3NeT collaboration "
