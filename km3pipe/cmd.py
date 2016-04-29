e5942a28587a86db629e1a6bcdf1515c09c33980
Tamas Gal <himself@tamasgal.com>
2016-04-29 10:10:44 +0200

    Make table sink standard

--- km3pipe/cmd.py
+++ km3pipe/cmd.py
@@ -5,7 +5,7 @@ KM3Pipe command line utility.
 
 Usage:
     km3pipe test
-    km3pipe tohdf5 [-s] -i FILE -o FILE
+    km3pipe tohdf5 -i FILE -o FILE
     km3pipe (-h | --help)
     km3pipe --version
 
@@ -13,7 +13,6 @@ Options:
     -h --help  Show this screen.
     -i FILE    Input file.
     -o FILE    Output file.
-    -s         Write each event in a separate dataset.
 
 """
 from __future__ import division, absolute_import, print_function
@@ -21,16 +20,14 @@ from __future__ import division, absolute_import, print_function
 from km3pipe import version
 
 
-def tohdf5(input_file, output_file, use_tables=True):
+def tohdf5(input_file, output_file):
     """Convert ROOT file to HDF5 file"""
     from km3pipe import Pipeline  # noqa
-    from km3pipe.pumps import AanetPump, HDF5Sink, HDF5TableSink  # noqa
-
-    sink = HDF5TableSink if use_tables else HDF5Sink
+    from km3pipe.pumps import AanetPump, HDF5TableSink  # noqa
 
     pipe = Pipeline()
     pipe.attach(AanetPump, filename=input_file)
-    pipe.attach(sink, filename=output_file)
+    pipe.attach(HDF5TableSink, filename=output_file)
     pipe.drain()
 
 
@@ -39,4 +36,4 @@ def main():
     arguments = docopt(__doc__, version=version)
 
     if arguments['tohdf5']:
-        tohdf5(arguments['-i'], arguments['-o'], arguments['-s'])
+        tohdf5(arguments['-i'], arguments['-o'])
