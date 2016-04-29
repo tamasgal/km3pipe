506bae5d23bec47519b6b497a4b4cbfca1fee618
Tamas Gal <himself@tamasgal.com>
2016-04-29 15:11:57 +0200

    Remove old junk

--- km3pipe/dataclasses.pyx
+++ km3pipe/dataclasses.pyx
@@ -19,8 +19,7 @@ np.import_array()
 
 from km3pipe.tools import angle_between
 
-__all__ = ('Point', 'Position', 'Direction', 'HitSeries', 'HitSeriesA', 'Hit',
-           'CPosition', 'CHitSeries')
+__all__ = ('Point', 'Position', 'Direction', 'HitSeries', 'Hit')
 
 
 point_dt = np.dtype([('x', float), ('y', float), ('z', float)])
@@ -121,35 +120,7 @@ class Direction_(Point_):
         return "({0:.4}, {1:.4}, {2:.4})".format(self.x, self.y, self.z)
 
 
-class HitSeriesA(object):
-    def __init__(self, data=None):
-        self.hit_dtype = np.dtype([
-            ('id', int),
-            ('dom_id', int),
-            ('time', int),
-            ('tot', int),
-            ('channel_id', int),
-            ('triggered', bool),
-            ('pmt_id', int),
-            ('t0', float),
-            ('pos_x', float),
-            ('pos_y', float),
-            ('pos_z', float),
-            ('dir_x', float),
-            ('dir_y', float),
-            ('dir_z', float),
-            ])
-        self._data = np.rec.array(data, dtype=self.hit_dtype)
-
-    @classmethod
-    def from_aanet(cls, data):
-        return cls(data=[(h.id, h.dom_id, h.t, h.tot, ord(h.channel_id),
-                          h.trig, h.pmt_id, np.nan,
-                          np.nan, np.nan, np.nan,
-                          np.nan, np.nan, np.nan) for h in data])
-
-
-cdef class CyHit:
+cdef class Hit:
     """Represents a hit on a PMT.
 
     Parameters
@@ -181,26 +152,7 @@ cdef class CyHit:
         self.pmt_id = pmt_id
 
 
-class CPosition(ctypes.Structure):
-    _fields_ = [('x', ctypes.c_float),
-                ('y', ctypes.c_float),
-                ('z', ctypes.c_float)]
-
-
-class CHit(ctypes.Structure):
-    _fields_ = [
-            ('id', ctypes.c_float),
-            ('dom_id', ctypes.c_float),
-            ('time', ctypes.c_float),
-            ('tot', ctypes.c_float),
-            ('channel_id', ctypes.c_float),
-            ('triggered', ctypes.c_bool),
-            ('pmt_id', ctypes.c_float),
-            ('pos', CPosition),
-            ]
-
-
-class CHitSeries(object):
+class HitSeries(object):
     def __init__(self, hits):
         self._hits = hits
         self._channel_id = None
@@ -213,20 +165,15 @@ class CHitSeries(object):
         self._index = 0
 
     @classmethod
-    def from_aanet_as_chit(cls, hits):
-        return cls([CHit(h.id, h.dom_id, h.t, h.tot, ord(h.channel_id),
-                    h.trig, h.pmt_id) for h in hits])
-
-    @classmethod
     def from_aanet(cls, hits):
-        return cls([CyHit(h.id, h.dom_id, h.t, h.tot, ord(h.channel_id),
+        return cls([Hit(h.id, h.dom_id, h.t, h.tot, ord(h.channel_id),
                     h.trig, h.pmt_id) for h in hits])
 
     @classmethod
     def from_arrays(cls, ids, dom_ids, times, tots, channel_ids,
                     triggereds, pmt_ids):
         args = ids, dom_ids, times, tots, channel_ids, triggereds, pmt_ids
-        hits = cls([CyHit(*hit_args) for hit_args in zip(*args)])
+        hits = cls([Hit(*hit_args) for hit_args in zip(*args)])
         hits._id = ids
         hits._time = times
         hits._id = ids
@@ -331,172 +278,3 @@ class CHitSeries(object):
 
 class TrackSeries(object):
     pass
-
-class HitSeries(object):
-    @classmethod
-    def from_hdf5(cls, data):
-        return cls(data, Hit.from_dict)
-
-    @classmethod
-    def from_aanet(cls, data):
-        return cls(data, Hit.from_aanet)
-
-    @classmethod
-    def from_evt(cls, data):
-        return cls(data, Hit.from_evt)
-
-    @classmethod
-    def from_dict(cls, data):
-        return cls(data, Hit.from_dict)
-
-    def __init__(self, data=None, hit_constructor=None):
-        self._data = data
-        self.hit_constructor = hit_constructor
-        self._hits = None
-        self._pos = None
-        self._dir = None
-        self._index = 0
-
-        if data is None:
-            self._hits = []
-
-    def append(self, hit):
-        if self._hits is None:
-            self._convert_hits()
-        self._hits.append(hit)
-        self._pos = None
-        self._dir = None
-
-    @property
-    def pos(self):
-        if self._hits is None:
-            self._convert_hits()
-        if self._pos is None:
-            self._pos = np.array([hit.pos for hit in self._hits])
-        return self._pos
-
-    @property
-    def dir(self):
-        if self._hits is None:
-            self._convert_hits()
-        if self._dir is None:
-            self._dir = np.array([hit.dir for hit in self._hits])
-        return self._dir
-
-    @property
-    def triggered(self):
-        """Return a copy of triggered hits."""
-        if self._hits is None:
-            self._convert_hits()
-        triggered_hits = [hit for hit in self._hits if hit.triggered]
-        return HitSeries(triggered_hits, Hit.from_hit)
-
-    def _convert_hits(self):
-        self._hits = [self.hit_constructor(hit) for hit in self._data]
-        self._data = None  # get rid of reference to allow GC
-
-    def __iter__(self):
-        return self
-
-    def next(self):
-        """Python 2/3 compatibility for iterators"""
-        return self.__next__()
-
-    def __next__(self):
-        if self._hits is None:
-            self._convert_hits()
-
-        if self._index >= len(self):
-            self._index = 0
-            raise StopIteration
-        item = self._hits[self._index]
-        self._index += 1
-        return item
-
-    def __len__(self):
-        if self._hits is None:
-            self._convert_hits()
-        return len(self._hits)
-
-    def __getitem__(self, index):
-        if self._hits is None:
-            self._convert_hits()
-
-        if isinstance(index, int):
-            return self._hits[index]
-        elif isinstance(index, slice):
-            return self._slice_generator(index)
-        else:
-            raise TypeError("index must be int or slice")
-
-    def _slice_generator(self, index):
-        """A simple slice generator for iterations"""
-        start, stop, step = index.indices(len(self))
-        for i in range(start, stop, step):
-            yield self._hits[i]
-
-    def __str__(self):
-        n_hits = len(self)
-        plural = 's' if n_hits > 1 or n_hits == 0 else ''
-        return("HitSeries with {0} hit{1}.".format(len(self), plural))
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __insp__(self):
-        if self._hits is None:
-            self._convert_hits()
-        return '\n'.join([str(hit) for hit in self._hits])
-
-
-class Hit(object):
-    def __init__(self, id=None, time=None, tot=None, channel_id=None,
-                 dom_id=None, pmt_id=None, triggered=None, data=None):
-        self.id = id
-        self.time = time
-        self.t0 = None
-        self.tot = tot
-        self.channel_id = channel_id
-        self.dom_id = dom_id
-        self.pmt_id = pmt_id
-        self.triggered = triggered
-        self.data = data
-        self.pos = None
-        self.dir = None
-        self.a = None  # charge <- historical
-
-    @classmethod
-    def from_hit(cls, hit):
-        new_hit = Hit(hit.id, hit.time, hit.tot, hit.channel_id, hit.dom_id,
-                      hit.pmt_id, hit.triggered, data=None)
-        if hit.pos is not None:
-            new_hit.pos = Position(hit.pos)
-        if hit.dir is not None:
-            new_hit.dir = Direction(hit.dir)
-        new_hit.a = hit.a
-        return hit
-
-    @classmethod
-    def from_dict(cls, data):
-        return cls(data['id'], data['time'], data['tot'], data['channel_id'],
-                   data['dom_id'], data=data)
-
-    @classmethod
-    def from_aanet(cls, data):
-        try:
-            return cls(data.id, data.t, data.tot, ord(data.channel_id),
-                       data.dom_id, triggered=bool(data.trig), data=data)
-        except TypeError:
-            return cls(data.id, data.t, data.tot, data.channel_id,
-                       data.dom_id, triggered=bool(data.trig), data=data)
-
-    @classmethod
-    def from_evt(cls, data):
-        return cls(data.id, data.time, data.tot, pmt_id=data.pmt_id, data=data)
-
-    def __str__(self):
-        return("Hit(id={0}, time={1}, tot={2}, triggered={3})"
-               .format(self.id, self.time, self.tot, self.triggered))
-
-    def __repr__(self):
-        return self.__str__()
