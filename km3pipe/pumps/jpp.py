ece254a6acfe18999e6451dee3fba162dc4a198b
Tamas Gal <himself@tamasgal.com>
2016-05-25 10:15:00 +0200

    Shiny new jppp pump

--- km3pipe/pumps/jpp.py
+++ km3pipe/pumps/jpp.py
@@ -8,7 +8,10 @@ Pump for the jpp file read through aanet interface.
 """
 from __future__ import division, absolute_import, print_function
 
+import numpy as np
+
 from km3pipe import Pump
+from km3pipe.dataclasses import HitSeries
 from km3pipe.logger import logging
 
 log = logging.getLogger(__name__)  # pylint: disable=C0103
@@ -20,57 +23,37 @@ class JPPPump(Pump):
     def __init__(self, **context):
         super(self.__class__, self).__init__(**context)
 
+        import jppp  # noqa
+
         self.index = self.get('index') or 0
-
-        import aa  # noqa
-        import ROOT
-        if self.get('index'):
-            self.index = self.get('index')
-        else:
-            self.index = 0
-
-        self.index_start = self.get('index_start') or 1
-        self.index_stop = self.get('index_stop') or 1
-
         self.filename = self.get('filename')
-        self.basename = self.get('basename')
-        if not self.filename and not self.basename:
-            raise ValueError("No filename defined")
 
-        self.file_index = self.index_start
-
-        if self.basename:
-            filename = self.basename + str(self.file_index) + ".JTE.root"
-            self.rootfile = ROOT.EventFile(filename)
-
-        else:
-                self.rootfile = ROOT.EventFile(self.filename)
-
-        self.evt = ROOT.Evt()
-
-    def get_blob(self, index):
-        """Return the blob"""
-        self.rootfile.set_index(index)
-        self.evt = self.rootfile.evt
-        return {'Evt': self.evt}
+        self.reader = jppp.PyJDAQEventReader(self.filename)
 
     def process(self, blob):
-        if self.rootfile.set_index(self.index):
-            self.evt = self.rootfile.evt
+        while self.reader.has_next():
+            self.reader.retrieve_next_event()
             self.index += 1
-            return {'Evt': self.evt}
-        else:
-            self.file_index += 1
-            if self.basename and self.file_index <= self.index_stop:
-                import aa  # noqa
-                import ROOT
-                print("open next file")
-                filename = self.basename + str(self.file_index) + ".JTE.root"
-                self.rootfile = ROOT.EventFile(filename)
-                self.index = 0
-                self.process(blob)
-            else:
-                raise StopIteration
+            print("Grabbing event with frame index {0}"
+                  .format(self.reader.get_frame_index()))
 
-    def finish(self):
-        self.rootfile.Close()
+            n = self.reader.get_number_of_snapshot_hits()
+            channel_ids = np.zeros(n, dtype='i')
+            dom_ids = np.zeros(n, dtype='i')
+            times = np.zeros(n, dtype='i')
+            tots = np.zeros(n, dtype='i')
+
+            self.reader.get_hits(channel_ids, dom_ids, times, tots)
+
+            hit_series = HitSeries.from_arrays(
+                channel_ids, dom_ids, np.arange(n), np.zeros(n), times, tots,
+                np.zeros(n), self.index
+            )
+
+            return {'FrameIndex': self.reader.get_frame_index(),
+                    'tots_arr': tots,
+                    'times_arr': times,
+                    'dom_ids_arr': dom_ids,
+                    'channel_ids_arr': channel_ids,
+                    'Hits': hit_series}
+        raise StopIteration
