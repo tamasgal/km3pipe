1b559a5e66083051781ed102273ae37ea76a2988
Tamas Gal <himself@tamasgal.com>
2016-06-22 09:08:10 +0200

    Add GenericPump

--- km3pipe/__init__.py
+++ km3pipe/__init__.py
@@ -22,6 +22,7 @@ if not __KM3PIPE_SETUP__:
     from km3pipe import srv  # noqa
     from km3pipe.srv import srv_event  # noqa
     from km3pipe.tools import read_hdf5, open_hdf5, read_reco  # noqa
+    from km3pipe.pumps import GenericPump
 
 __author__ = "Tamas Gal and Moritz Lotze"
 __copyright__ = "Copyright 2016, Tamas Gal and the KM3NeT collaboration."
--- km3pipe/pumps/__init__.py
+++ km3pipe/pumps/__init__.py
@@ -6,6 +6,8 @@ A collection of pumps for different kinds of data formats.
 """
 from __future__ import division, absolute_import, print_function
 
+import os
+
 from km3pipe.pumps.evt import EvtPump  # noqa
 from km3pipe.pumps.daq import DAQPump  # noqa
 from km3pipe.pumps.clb import CLBPump  # noqa
@@ -15,3 +17,35 @@ from km3pipe.pumps.ch import CHPump  # noqa
 from km3pipe.pumps.hdf5 import HDF5Pump  # noqa
 from km3pipe.pumps.hdf5 import HDF5Sink  # noqa
 from km3pipe.pumps.pickle import PicklePump  # noqa
+
+from km3pipe.logger import logging
+
+__author__ = "Tamas Gal"
+__copyright__ = "Copyright 2016, Tamas Gal and the KM3NeT collaboration."
+__credits__ = []
+__license__ = "MIT"
+__maintainer__ = "Tamas Gal"
+__email__ = "tgal@km3net.de"
+__status__ = "Development"
+
+
+log = logging.getLogger(__name__)
+
+
+def GenericPump(filename, use_jppy=False):
+    """A generic pump which utilises the appropriate pump."""
+    extension = os.path.splitext(filename)[1]
+
+    pumps = {
+            '.evt': EvtPump,
+            '.h5': HDF5Pump,
+            '.aa.root': AanetPump,
+            '.root': JPPPump if use_jppy else AanetPump,
+            '.dat': DAQPump,
+            '.dqd': CLBPump,
+            }
+
+    if extension not in pumps:
+        log.critical("No pump found for '{0}'".format(extension))
+
+    return pumps[extension](filename=filename)
