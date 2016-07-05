18db2ab17d3f114e0cecec5b8d49621bfd769132
Moritz Lotze <lotzemoritz@gmail.com>
2016-07-05 12:42:37 +0200

    add extra-dep (optional) dep sklearn. does nothing so far, just a reminder for future me

--- setup.py
+++ setup.py
@@ -69,6 +69,9 @@ setup(name='km3pipe',
           'mock',
           'websocket-client',
       ],
+      extra_require = {
+          'scikit-learn': ['sklearn'],
+      },
       entry_points={
           'console_scripts': [
               'km3pipe=km3pipe.cmd:main',
