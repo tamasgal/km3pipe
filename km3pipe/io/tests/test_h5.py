3bf17fdc973137c3b8830e202292b92117b32aea
Moritz Lotze <lotzemoritz@gmail.com>
2016-08-30 17:08:59 +0200

    start WriteRead test

--- km3pipe/io/tests/test_h5.py
+++ km3pipe/io/tests/test_h5.py
@@ -5,8 +5,10 @@ import os
 import numpy as np
 import tables as tb
 
-from km3pipe.testing import TestCase
+from km3pipe import Pipeline
 from km3pipe.io import _insert_tabname_into_colnames, _read_group
+from km3pipe.io.hdf5 import HDF5Pump, HDF5Sink
+from km3pipe.testing import TestCase
 
 
 class TestMultiTable(TestCase):
@@ -53,3 +55,27 @@ class TestMultiTable(TestCase):
         print(res_cols)
         self.assertEqual(exp_shape, res_shape)
         self.assertEqual(exp_cols, res_cols)
+
+
+class TestWriteRead(TestCase):
+    def setUp(self):
+        self.foo = np.array([
+            (1.0, 2.0, 3.0),
+            (4.0, 5.0, 6.0),
+        ], dtype=[('a', '<f8'), ('b', '<f8'), ('c', '<f8'), ])
+        self.bar = np.array([
+            (10.0, 20.0, 30.0),
+            (40.0, 50.0, 60.0),
+        ], dtype=[('aa', '<f8'), ('bb', '<f8'), ('cc', '<f8'), ])
+        self.tabs = {'foo': self.foo, 'bar': self.bar}
+        self.where = '/lala'
+        self.h5name = './test.h5'
+        self.h5file = tb.open_file(
+            # create the file in memory only
+            self.h5name, 'w', driver="H5FD_CORE", driver_core_backing_store=0)
+        for name, tab in self.tabs.items():
+            self.h5file.create_table(self.where, name=name, obj=tab,
+                                     createparents=True)
+
+    def tearDown(self):
+        self.h5file.close()
