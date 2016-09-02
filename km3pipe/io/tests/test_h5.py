cae37f9beb21660a507ec2aeba9c4467b043f405
Moritz Lotze <lotzemoritz@gmail.com>
2016-09-02 16:14:45 +0200

    improve H5Chain testing

--- km3pipe/io/tests/test_h5.py
+++ km3pipe/io/tests/test_h5.py
@@ -60,32 +60,60 @@ class TestMultiTable(TestCase):
 class TestH5Chain(TestCase):
     def setUp(self):
         self.foo = np.array([
-            (1.0, 2.0, 3.0),
-            (4.0, 5.0, 6.0),
-        ], dtype=[('a', '<f8'), ('b', '<f8'), ('c', '<f8'), ])
+            (1.0, 2.0, 1.0, 1),
+            (4.0, 5.0, 0.0, 1),
+            (1.2, 2.2, 1.2, 2),
+            (4.2, 5.2, 0.2, 2),
+            (1.5, 2.5, 1.5, 3),
+            (4.5, 5.5, 0.5, 3),
+            (1.5, 2.5, 1.5, 4),
+            (4.5, 5.5, 0.5, 4),
+        ], dtype=[('a', '<f8'), ('b', '<f8'),
+                  ('c', '<f8'), ('event_id', int)])
         self.bar = np.array([
-            (10.0, 20.0, 30.0),
-            (40.0, 50.0, 60.0),
-        ], dtype=[('aa', '<f8'), ('bb', '<f8'), ('cc', '<f8'), ])
+            (10.5, 20.5, 30.5, 1),
+            (40.5, 50.5, 60.5, 2),
+            (11.5, 21.5, 31.5, 3),
+            (41.5, 51.5, 61.5, 4),
+        ], dtype=[('aa', '<f8'), ('bb', '<f8'),
+                  ('cc', '<f8'), ('event_id', int)])
         self.yay = np.array([
-            (10.0, 20.0, 30.0),
-            (40.0, 50.0, 60.0),
-        ], dtype=[('aaa', '<f8'), ('bbb', '<f8'), ('ccc', '<f8'), ])
+            (10.0, 20.0, 30.0, 1),
+            (40.0, 50.0, 60.0, 2),
+            (12.0, 22.0, 32.0, 3),
+            (42.0, 52.0, 62.0, 4),
+        ], dtype=[('aaa', '<f8'), ('bbb', '<f8'),
+                  ('ccc', '<f8'), ('event_id', int)])
         self.tabs = {'foo': self.foo, 'bar': self.bar, 'yay': self.yay}
         self.where = {'foo': '/', 'bar': '/lala', 'yay': '/lala'}
         self.h5name = './test.h5'
+        self.h5name2 = './test2.h5'
         self.h5file = tb.open_file(self.h5name, 'a')
+        self.h5file2 = tb.open_file(self.h5name2, 'a')
         for name, tab in self.tabs.items():
             self.h5file.create_table(self.where[name], name=name, obj=tab,
                                      createparents=True)
+            self.h5file2.create_table(self.where[name], name=name, obj=tab,
+                                      createparents=True)
         self.h5file.close()
+        self.h5file2.close()
 
     def tearDown(self):
         os.remove(self.h5name)
+        os.remove(self.h5name2)
 
-    def test_single_file(self):
-        files = {self.h5name: 1}
+    def test_two_files(self):
+        files = {self.h5name: None, self.h5name2: None}
         c = H5Chain(files)
-        print(c['foo'])
+        print(c['foo'][::2])
         print(c['lala'])
         self.assertTrue(c['foo'].equals(c.foo))
+        self.assertTrue(False)
+
+    # def test_slice(self):
+    #     files = {self.h5name: slice(None, None, 2)}
+    #     c = H5Chain(files)
+    #     self.assertTrue(c['foo'].equals(c.foo))
+    #     files = {self.h5name: slice(1, 3, 2)}
+    #     c = H5Chain(files)
+    #     self.assertTrue(c['foo'].equals(c.foo))
