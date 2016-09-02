8a852b45ae4f7c27a9e96883dff40c2618e9c3a5
Moritz Lotze <lotzemoritz@gmail.com>
2016-09-02 15:01:35 +0200

    add test env

--- km3pipe/io/tests/test_h5.py
+++ km3pipe/io/tests/test_h5.py
@@ -5,9 +5,8 @@ import os
 import numpy as np
 import tables as tb
 
-from km3pipe import Pipeline
-from km3pipe.io import _insert_tabname_into_colnames, _read_group
-from km3pipe.io.hdf5 import HDF5Pump, HDF5Sink
+from km3pipe.io import _insert_prefix_to_dtype, _read_group
+from km3pipe.io.hdf5 import H5Chain
 from km3pipe.testing import TestCase
 
 
@@ -37,8 +36,8 @@ class TestMultiTable(TestCase):
     def test_name_insert(self):
         exp_foo = ('foo_a', 'foo_b', 'foo_c')
         exp_bar = ('bar_aa', 'bar_bb', 'bar_cc')
-        pref_foo = _insert_tabname_into_colnames(self.tabs['foo'], 'foo')
-        pref_bar = _insert_tabname_into_colnames(self.tabs['bar'], 'bar')
+        pref_foo = _insert_prefix_to_dtype(self.tabs['foo'], 'foo')
+        pref_bar = _insert_prefix_to_dtype(self.tabs['bar'], 'bar')
         self.assertEqual(exp_foo, pref_foo.dtype.names)
         self.assertEqual(exp_bar, pref_bar.dtype.names)
 
@@ -57,7 +56,8 @@ class TestMultiTable(TestCase):
         self.assertEqual(exp_cols, res_cols)
 
 
-class TestWriteRead(TestCase):
+
+class TestH5Chain(TestCase):
     def setUp(self):
         self.foo = np.array([
             (1.0, 2.0, 3.0),
@@ -67,15 +67,24 @@ class TestWriteRead(TestCase):
             (10.0, 20.0, 30.0),
             (40.0, 50.0, 60.0),
         ], dtype=[('aa', '<f8'), ('bb', '<f8'), ('cc', '<f8'), ])
-        self.tabs = {'foo': self.foo, 'bar': self.bar}
-        self.where = '/lala'
+        self.yay = np.array([
+            (10.0, 20.0, 30.0),
+            (40.0, 50.0, 60.0),
+        ], dtype=[('aaa', '<f8'), ('bbb', '<f8'), ('ccc', '<f8'), ])
+        self.tabs = {'foo': self.foo, 'bar': self.bar, 'yay': self.yay}
+        self.where = {'foo': '/', 'bar': '/lala', 'yay': '/lala'}
         self.h5name = './test.h5'
-        self.h5file = tb.open_file(
-            # create the file in memory only
-            self.h5name, 'w', driver="H5FD_CORE", driver_core_backing_store=0)
+        self.h5file = tb.open_file(self.h5name, 'a')
         for name, tab in self.tabs.items():
             self.h5file.create_table(self.where, name=name, obj=tab,
                                      createparents=True)
-
-    def tearDown(self):
         self.h5file.close()
+
+    # def tearDown(self):
+    #     os.remove(self.h5name)
+
+    def test_single_file(self):
+        files = {self.h5name: 1}
+        c = H5Chain(files)
+        print(c['foo'])
+        self.assertTrue(False)
