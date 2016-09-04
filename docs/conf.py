83f5de29fb8d7f0c3e960aab1230180d86f3ea3b
Moritz Lotze <lotzemoritz@gmail.com>
2016-09-04 14:14:21 +0200

    DOC: use sphinx-better theme + custom CSS

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,7 @@ import sys
 import os
 from datetime import date
 
-import sphinx_rtd_theme
+from better import better_theme_path
 
 sys.path.append('../')
 
@@ -126,22 +126,25 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'better'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-# html_theme_options = {}
+html_theme_options = {
+    'cssfiles': ['_static/better.css'],
+    'textcolor': '#444',
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [better_theme_path]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 # html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-# html_short_title = None
+html_short_title = 'Home'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -171,7 +174,10 @@ html_static_path = ['_static']
 # html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-# html_sidebars = {}
+html_sidebars = {
+    '**': ['globaltoc.html', 'searchbox.html'],
+}
+
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
