e376981565acccb69faaf2357b85ec9ab5299f03
Tamas Gal <himself@tamasgal.com>
2016-10-08 10:36:08 +0200

    Use one table for each timeslice instead of separate tables for their frames

--- km3pipe/dataclasses.pyx
+++ km3pipe/dataclasses.pyx
@@ -775,11 +775,12 @@ class L0HitSeries(object):
 
     @property
     def h5loc(self):
-        return "/timeslices/slice_{0}".format(self.slice_id)
+        #return "/timeslices/slice_{0}".format(self.slice_id)
+        return "/timeslices"
 
     @property
     def tabname(self):
-        return "frame_{0}".format(self.frame_id)
+        return "{0}".format(self.slice_id)
 
     @classmethod
     def from_arrays(cls, channel_ids, dom_ids, times, tots, slice_id, frame_id):
