aa9c1e093755fdc3013082ec6823ce18b4d31fd5
Tamas Gal <himself@tamasgal.com>
2016-10-12 16:38:41 +0200

    Call style.use explicitly

--- examples/plot_tot_histogram.py
+++ examples/plot_tot_histogram.py
@@ -8,7 +8,9 @@ This example shows how to create a simple histogram of the PMT signals.
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import km3pipe.style
+import km3pipe as kp
+
+plt.style.use(kp.mplstyle)
 
 filename = "data/km3net_jul13_90m_muatm50T655.km3_v5r1.JTE_r2356.root.0-499.h5"
 
