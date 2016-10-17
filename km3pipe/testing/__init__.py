3cca279260029a671773ea06eb3e08f7bfcb51a2
Moritz Lotze <lotzemoritz@gmail.com>
2016-10-18 00:01:14 +0200

    aanet fakeVec

--- km3pipe/testing/__init__.py
+++ km3pipe/testing/__init__.py
@@ -30,16 +30,22 @@ __email__ = "tgal@km3net.de"
 __status__ = "Development"
 
 
+class FakeVec(object):
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
 class FakeAanetHit(object):
     def __init__(self, channel_id, dir_x, dir_y, dir_z, dom_id, id, pmt_id,
                  pos_x, pos_y, pos_z, t0, t, tot, trig):
         # self.channel_id = chr(channel_id)
         self.channel_id = channel_id
-        self.dir = (dir_x, dir_y, dir_z)
+        self.dir = FakeVec(dir_x, dir_y, dir_z)
         self.dom_id = dom_id
         self.id = id
         self.pmt_id = pmt_id
-        self.pos = (pos_x, pos_y, pos_z)
+        self.pos = FakeVec(pos_x, pos_y, pos_z)
         self.t0 = t0
         self.t = t
         self.tot = tot
