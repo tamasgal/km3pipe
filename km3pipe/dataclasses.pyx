203fa78ab8fe08885bf3bd84028075d37a32a51e
Moritz Lotze <lotzemoritz@gmail.com>
2016-11-04 18:05:23 +0100

    remove obsolete sorter

--- km3pipe/dataclasses.pyx
+++ km3pipe/dataclasses.pyx
@@ -870,18 +870,10 @@ class HitSeries(object):
         return HitSeries(self._arr[self._arr['triggered'] == True])
 
     @property
-    def time_sorted(self):
-        """Sort hits by (calibrated) time."""
-        h = self.serialise(to='pandas')
-        h['calib_time'] = h['time'] - h['t0']
-        h.sort_values('calib_time', inplace=True)
-        h.drop('calib_time', axis=1, inplace=True)
-        return HitSeries(h.to_records(index=False))
-
-    @property
     def first_hits(self):
-        h = self.time_sorted.serialise(to='pandas')
-        fh = h.drop_duplicates(subset='dom_id')
+        h = self.serialise(to='pandas')
+        h.sort_values('time', inplace=True)
+        h = h.drop_duplicates(subset='dom_id')
         return HitSeries(fh.to_records(index=False))
 
     @property
