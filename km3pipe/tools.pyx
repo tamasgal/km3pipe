65f49fb433f1992126d90aa9273b7d710e64d9a7
Moritz Lotze <lotzemoritz@gmail.com>
2016-11-16 17:37:21 +0100

    add distance measure

--- km3pipe/tools.pyx
+++ km3pipe/tools.pyx
@@ -152,6 +152,14 @@ def pld3(p1, p2, d2):
     return scipy.linalg.norm(np.cross(d2, p2 - p1)) / scipy.linalg.norm(d2)
 
 
+def lpnorm(x, p=2):
+    return np.power(np.sum(np.power(x, p)), 1/p)
+
+
+def dist(x1, x2):
+    return lpnorm(x2 - x1, p=2)
+
+
 def com(points, masses=None):
     """Calculate center of mass for given points.
     If masses is not set, assume equal masses."""
