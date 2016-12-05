c6dd8293cc7d5446d007f0ac9b7931852353ab5d
Tamas Gal <himself@tamasgal.com>
2016-12-05 15:50:41 +0100

    Add pygments and nbconvert style

--- km3pipe/style.py
+++ km3pipe/style.py
@@ -12,6 +12,7 @@ import os
 import matplotlib.pyplot as plt
 import km3pipe as kp
 
+
 __author__ = "Tamas Gal"
 __copyright__ = "Copyright 2016, Tamas Gal and the KM3NeT collaboration."
 __credits__ = []
@@ -22,6 +23,43 @@ __status__ = "Development"
 
 
 style_dir = os.path.dirname(kp.__file__) + '/kp-data/stylelib'
+
+
+class KM3PipePygmentsStyle(object):
+    def __init__(self):
+        raise ImportError("Module pygments could be not found")
+
+class LatexPreprocessor(object):
+    def __init__(self):
+        raise ImportError("Module nbconvert could be not found")
+
+try:
+    from nbconvert.preprocessors.base import Preprocessor
+    from pygments.style import Style
+    import pygments.token as token
+except ImportError:
+    pass
+else:
+    class KM3PipePygmentsStyle(Style):
+        default_style = ""
+        styles = {
+            token.Comment:         '#aaa',
+            token.Keyword:         '#0095B5',
+            token.Keyword.Control: '#0095B5',
+            token.Name:            '#333',
+            token.Name.Function:   '#FF0082',
+            token.Name.Class:      'bold #FF0082',
+            token.String:          '#666',
+            token.Operator:        '#346F8A',
+        }
+
+    class LatexPreprocessor(Preprocessor):
+        """LaTeX processor for nbconvert"""
+        def preprocess(self, nb, resources):
+            from pygments.formatters import LatexFormatter
+            resources["latex"]["pygments_definitions"] = \
+                    LatexFormatter(style=KM3PipePygmentsStyle).get_style_defs()
+            return nb, resources
 
 
 def get_style_path(style='km3pipe'):
