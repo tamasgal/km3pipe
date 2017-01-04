fc4b43083c5e9217f04961dd4622b06c0365d3e5
Moritz Lotze <lotzemoritz@gmail.com>
2017-01-04 17:45:02 +0100

    mc mod: grab most energetic primary, optionally

--- km3modules/mc.py
+++ km3modules/mc.py
@@ -12,9 +12,15 @@ NEUTRINOS = {'nu_e', 'anu_e', 'nu_mu', 'anu_mu', }      # noqa
 
 
 class McTruth(Module):
-    """Extract MC info of 1st MC track."""
+    """Extract MC info of 1st MC track.
+
+    Parameters
+    ----------
+    most_energetic_primary: bool, default=True
+    """
     def __init__(self, **context):
         super(self.__class__, self).__init__(**context)
+        self.most_energetic_primary = bool(self.get('most_energetic_primary')) or True
 
     @classmethod
     def t2f(cls, row):
@@ -25,7 +31,10 @@ class McTruth(Module):
         return flavor in NEUTRINOS
 
     def process(self, blob):
-        mc = blob['McTracks'].conv_to('pandas').head(1)
+        mc = blob['McTracks'].conv_to('pandas')
+        if self.most_energetic_primary:
+            mc.sort_values('energy', ascending=False, inplace=True)
+        mc = mc.head(1)
         mc['zenith'] = zenith(mc[['dir_x', 'dir_y', 'dir_z']])
         mc['azimuth'] = azimuth(mc[['dir_x', 'dir_y', 'dir_z']])
         flavor = mc.apply(self.t2f, axis=1)
