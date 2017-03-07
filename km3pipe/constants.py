18243dab60d843cc108164afeb672bf3e00e95fc
Moritz Lotze <lotzemoritz@gmail.com>
2017-03-07 12:09:00 +0100

    add arca coordinates

--- km3pipe/constants.py
+++ km3pipe/constants.py
@@ -47,3 +47,7 @@ dt_window_h = +25  # ns
 orca_longitude = 42 + (48/60)  # degree
 orca_latitude = 6 + (2/60)  # degree
 orca_height = -2450  # m
+
+arca_longitude = 36 + (16/60)  # degree
+arca_latitude = 10 + (6/60)  # degree
+arca_height = -3500  # m
