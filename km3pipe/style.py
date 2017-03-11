7c0ed3f3b65a4189b0089be66a9ad8a649ff8f0f
Tamas Gal <himself@tamasgal.com>
2017-03-11 19:52:05 +0100

    Warn if matplotlib not found

--- km3pipe/style.py
+++ km3pipe/style.py
@@ -10,7 +10,10 @@ from __future__ import division, absolute_import, print_function
 import os
 from itertools import cycle
 
-import matplotlib.pyplot as plt
+try:
+    import matplotlib.pyplot as plt
+except ImportError:
+    print("Please install matplotlib: `pip install matplotlib`")
 import km3pipe as kp
 
 
