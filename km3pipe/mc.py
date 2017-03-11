0607e1713ad80918b133085255c2c2be516b0131
Tamas Gal <himself@tamasgal.com>
2017-03-11 21:19:42 +0100

    Update docstring

--- km3pipe/mc.py
+++ km3pipe/mc.py
@@ -4,7 +4,7 @@
 # cython: embedsignature=True
 # pylint: disable=C0103
 """
-Some unsorted, frequently used logic.
+Monte Carlo related things.
 
 """
 from __future__ import division, absolute_import, print_function
