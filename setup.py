73cf371b63a7ba389542e29aca339b7e0784a9fa
Tamas Gal <himself@tamasgal.com>
2017-03-12 13:16:53 +0100

    Fake keyword arg

--- setup.py
+++ setup.py
@@ -21,8 +21,7 @@ try:
 except ImportError:
     numpy = lambda : None
     numpy.get_include = lambda : ''
-    cythonize = lambda x, y: ''
-    pass
+    def cythonize(a, compiler_directives): pass
 #    raise SystemExit("\nCython and Numpy are required to compile KM3Pipe.\n"
 #                     "You can install it easily via pip:\n\n"
 #                     "    > pip install cython numpy")
