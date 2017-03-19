6689e90800728ec25413d0915733bf54cb243bc8
Tamas Gal <tgal@km3net.de>
2017-03-19 21:58:28 +0100

    Update examples

---
+++ examples/monitoring/ligier_mirror.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding=utf-8
+# vim: ts=4 sw=4 et
+"""
+=============
+Ligier Mirror
+=============
+
+Subscribes to given tag(s) and sends them to another Ligier.
+
+"""
+# Author: Tamas Gal <tgal@km3net.de>
+# License: MIT
+from __future__ import division
+
+import socket
+
+from km3pipe import Pipeline, Module
+from km3pipe.io import CHPump
+import controlhost as ch
+
+from km3pipe.logger import logging
+
+
+class LigierSender(Module):
+    def configure(self):
+        self.ligier = self.get("ligier") or "127.0.0.1"
+        self.port = self.get("port") or 5553
+        self.socket = socket.socket()
+        self.client = self.socket.connect((self.ligier, self.port))
+
+    def process(self, blob):
+        self.socket.send(blob["CHPrefix"].data + blob["CHData"])
+
+    def finish(self):
+        self.socket.close()
+
+
+pipe = Pipeline()
+pipe.attach(CHPump, host='192.168.0.121',
+                    port=5553,
+                    tags='IO_EVT, IO_SUM, IO_TSL',
+                    timeout=60*60*24*7,
+                    max_queue=2000)
+pipe.attach(LigierSender)
+pipe.drain()
--- examples/network/udp_dispatcher.py
+++ examples/network/udp_dispatcher.py
@@ -8,9 +8,16 @@ UDP Forwared for ControlHost
 
 A simple UDP forwarder for ControlHost messages.
 """
-
 # Author: Tamas Gal <tgal@km3net.de>
 # License: MIT
+#!/usr/bin/env python
+# coding=utf-8
+# vim: ts=4 sw=4 et
+"""
+This application is used to forward monitoring channel data from Ligier
+to a given UDP address.
+
+"""
 from __future__ import print_function
 
 import socket
@@ -23,15 +30,17 @@ __email__ = "tgal@km3net.de"
 
 
 class UDPForwarder(kp.Module):
-    def __init__(self, **context):
-        super(self.__class__, self).__init__(**context)
+    def configure(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.counter = 0
 
     def process(self, blob):
         if str(blob['CHPrefix'].tag) == 'IO_MONIT':
-            self.sock.sendto(blob['CHData'], ('127.0.0.1', 50000))
-            #sys.stdout.write('.')
-            #sys.stdout.flush()
+            self.sock.sendto(blob['CHData'], ('127.0.0.1', 56017))
+            if self.counter % 100 == 0:
+                sys.stdout.write('.')
+                sys.stdout.flush()
+            self.counter += 1
         return blob
 
 
@@ -39,9 +48,10 @@ class UDPForwarder(kp.Module):
 pipe = kp.Pipeline()
 pipe.attach(kp.io.CHPump,
             host='localhost',
-            port=31286,
+            port=5553,
             tags='IO_MONIT',
             timeout=60*60*24*7,
-            max_queue=1000)
+            max_queue=1000,
+            timeit=True)
 pipe.attach(UDPForwarder)
 pipe.drain()
