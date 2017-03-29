1a514813b50366751b54425e05dbf86d949d0a3e
Moritz Lotze <lotzemoritz@gmail.com>
2017-03-29 19:48:23 +0200

    fix example pandas readout

--- examples/plot_tot_histogram.py
+++ examples/plot_tot_histogram.py
@@ -11,13 +11,15 @@ Create a simple histogram of the PMT signals (ToTs) in all events.
 
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from km3pipe.io.pandas import read_hdf_group
 import km3pipe.style
 km3pipe.style.use("km3pipe")
 
 
 filename = "data/km3net_jul13_90m_muatm50T655.km3_v5r1.JTE_r2356.root.0-499.h5"
 
-hits = pd.read_hdf(filename, 'hits')
+hits = read_hdf_group(filename, 'hits')
 hits.hist("tot", bins=254, log=True, edgecolor='none')
 plt.title("ToT distribution")
 plt.xlabel("ToT [ns]")
