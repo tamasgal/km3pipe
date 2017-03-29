1786d0c689397de4299375d2906f6464efbb5e73
Tamas Gal <tgal@km3net.de>
2017-03-29 20:04:48 +0200

    Monitoring channel demo

---
+++ examples/monitoring/io_monit.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+# vim: ts=4 sw=4 et
+"""
+==================
+Monitoring Channel
+==================
+
+This application demonstrates how to access monitoring channel data.
+
+"""
+# Author: Tamas Gal <tgal@km3net.de>
+# License: MIT
+#!/usr/bin/env python
+# coding=utf-8
+# vim: ts=4 sw=4 et
+from __future__ import print_function
+import io
+import km3pipe as kp
+
+__author__ = "Tamas Gal"
+__email__ = "tgal@km3net.de"
+
+
+def print_monitoring_data(blob):
+    data = io.BytesIO(blob['CHData'])
+    print(kp.io.daq.TMCHData(data))
+    return blob
+
+
+pipe = kp.Pipeline()
+pipe.attach(kp.io.CHPump, host='localhost', tags='IO_MONIT')
+pipe.attach(print_monitoring_data)
+pipe.drain()
