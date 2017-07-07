f48063cfb9674c1e5f1f94e62ff43b239f687abd
Tamas Gal <tgal@km3net.de>
2017-07-07 10:40:22 +0200

    Fix for new km3hdf5 version 4

--- examples/plot_tot_histogram.py
+++ examples/plot_tot_histogram.py
@@ -9,7 +9,7 @@ Create a simple histogram of the PMT signals (ToTs) in all events.
 # Author: Tamas Gal <tgal@km3net.de>
 # License: BSD-3
 
-import pandas as pd
+import tables as tb
 import matplotlib.pyplot as plt
 import km3pipe.style
 km3pipe.style.use("km3pipe")
@@ -17,7 +17,9 @@ km3pipe.style.use("km3pipe")
 
 filename = "data/km3net_jul13_90m_muatm50T655.km3_v5r1.JTE_r2356.root.0-499.h5"
 
-hits = pd.read_hdf(filename, 'hits', mode='r')
-hits.hist("tot", bins=254, log=True, edgecolor='none')
+with tb.File(filename) as f:
+    tots = f.get_node("/hits/tot")[:]
+
+plt.hist(tots, bins=254, log=True, edgecolor='none')
 plt.title("ToT distribution")
 plt.xlabel("ToT [ns]")
