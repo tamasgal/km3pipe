c115ab4f84e7fa6d4d9b703c664015471a9126be
Tamas Gal <tgal@km3net.de>
2017-10-13 18:33:09 +0200

    Update k40 calib example

--- examples/monitoring/k40_calibration.py
+++ examples/monitoring/k40_calibration.py
@@ -16,6 +16,8 @@ from km3pipe.io.daq import TMCHData
 from km3pipe.logger import logging
 from km3modules import k40
 from km3modules.common import StatusBar, MemoryObserver
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pylab as plt
 from collections import defaultdict
 import numpy as np
@@ -36,16 +38,15 @@ log_core = kp.logger.get("km3pipe.core")
 class IntraDOMCalibrationPlotter(kp.Module):
     def process(self, blob):
         calibration = blob["IntraDOMCalibration"]
-        #for process in (self.create_plot, self.save_hdf5):
-        #for process in (self.create_plot, self.save_hdf5):
-        proc = mp.Process(target=self.create_plot, args=(calibration,))
-        proc.daemon = True
-        proc.start()
-        proc.join()
+        for process in (self.create_plot, self.save_hdf5):
+            proc = mp.Process(target=process, args=(calibration,))
+            proc.daemon = True
+            proc.start()
+            proc.join()
 
     def create_plot(self, calibration):
         print("Creating plot...")
-        fig, axes = plt.subplots(8, 4, figsize=(16, 20),
+        fig, axes = plt.subplots(6, 3, figsize=(16, 20),
                                  sharex=True, sharey=True)
         for ax, (dom_id, calib) in zip(axes.flatten(), calibration.items()):
             ax.plot(np.cos(calib['angles']), calib["means"], '.')
@@ -53,10 +54,10 @@ class IntraDOMCalibrationPlotter(kp.Module):
             ax.set_title("{0} - {1}".format(db.doms.via_dom_id(dom_id), dom_id))
             ax.set_ylim((-20, 20))
         plt.suptitle("{0} UTC".format(datetime.utcnow().strftime("%c")))
-        plt.savefig("intradom.png", bbox_inches='tight')
+        plt.savefig("km3web/plots/intradom.png", bbox_inches='tight')
         plt.close('all')
 
-        fig, axes = plt.subplots(8, 4, figsize=(16, 20),
+        fig, axes = plt.subplots(6, 3, figsize=(16, 20),
                                  sharex=True, sharey=True)
         for ax, (dom_id, calib) in zip(axes.flatten(), calibration.items()):
             ax.plot(np.cos(calib['angles']), calib["rates"], '.')
@@ -64,13 +65,13 @@ class IntraDOMCalibrationPlotter(kp.Module):
             ax.set_title("{0} - {1}".format(db.doms.via_dom_id(dom_id), dom_id))
             ax.set_ylim((0, 15))
         plt.suptitle("{0} UTC".format(datetime.utcnow().strftime("%c")))
-        plt.savefig("angular_k40rate_distribution.png",
+        plt.savefig("km3web/plots/angular_k40rate_distribution.png",
                     bbox_inches='tight')
         plt.close('all')
 
     def save_hdf5(self, calibration):
         print("Saving calibration information...")
-        store = pd.HDFStore('data/k40calib.h5', mode='r')
+        store = pd.HDFStore('data/k40calib.h5', mode='a')
         now = int(time.time())
         timestamps = (now,) * 31
         for dom_id, calib in calibration.items():
@@ -94,28 +95,26 @@ def CHTagger(blob):
 
 
 
-class MeanPMTRatesService(kp.Module):
+class MedianPMTRatesService(kp.Module):
     def configure(self):
-        self.mean_rates = {}
         self.rates = defaultdict(lambda: defaultdict(list))
-        self.expose(self.mean_rates, 'MeanPMTRates')
-        self.index = 0
+        self.expose(self.get_median_rates, 'GetMedianPMTRates')
 
     def process(self, blob):
-        self.index += 1
         tmch_data = TMCHData(io.BytesIO(blob['CHData']))
         dom_id = tmch_data.dom_id
         for channel_id, rate in enumerate(tmch_data.pmt_rates):
             self.rates[dom_id][channel_id].append(rate)
-
-        if self.index % 10*31 == 0:
-            self._calculate_mean_rates()
         return blob
 
-    def _calculate_mean_rates(self):
+    def get_median_rates(self):
+        print("Calculating median PMT rates.")
+        median_rates = {}
         for dom_id in self.rates.keys():
-            self.mean_rates[dom_id] = [np.mean(self.rates[dom_id][c])  \
-                                       for c in range(31)]
+            median_rates[dom_id] = [np.median(self.rates[dom_id][c])  \
+                                  for c in range(31)]
+        self.rates = defaultdict(lambda: defaultdict(list))
+        return median_rates
 
 
 pipe = kp.Pipeline()
@@ -128,10 +127,13 @@ pipe.attach(kp.io.ch.CHPump,
 pipe.attach(CHTagger)
 pipe.attach(StatusBar, every=1000)
 pipe.attach(MemoryObserver, every=5000)
-pipe.attach(MeanPMTRatesService, only_if='IO_MONIT')
+pipe.attach(MedianPMTRatesService, only_if='IO_MONIT')
 pipe.attach(kp.io.daq.TimesliceParser, only_if='IO_TSL')
-pipe.attach(k40.CoincidenceFinder, accumulate=10*20, only_if='TimesliceFrames')
+pipe.attach(k40.CoincidenceFinder,
+            accumulate=10*60*20,
+            only_if='TimesliceFrames',
+            tmax=10)
 pipe.attach(k40.K40BackgroundSubtractor, only_if='K40Counts')
-pipe.attach(k40.IntraDOMCalibrator, only_if='K40Counts')
+pipe.attach(k40.IntraDOMCalibrator, only_if='K40Counts', fit_background=False)
 pipe.attach(IntraDOMCalibrationPlotter, only_if='IntraDOMCalibration')
 pipe.drain()
