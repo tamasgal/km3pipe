7502c52a3acc38fa9eb3c32d1ae59cdffbe2ab03
Tamas Gal <tgal@km3net.de>
2017-11-01 01:52:45 +0100

    Speedup apply

--- km3pipe/core.pyx
+++ km3pipe/core.pyx
@@ -19,6 +19,7 @@ import types
 
 import numpy as np
 import pandas as pd
+import numba as nb
 
 from .sys import peak_memory_usage, ignored
 from .hardware import Detector
@@ -497,6 +498,7 @@ class Geometry(Module):
             hits._arr['time'][idx] += pmt.t0
             # hit.a = hit.tot
 
+    @nb.jit
     def _apply_to_rawhitseries(self, hits):
         """Create a HitSeries from RawHitSeries and add pos, dir and t0.
         
