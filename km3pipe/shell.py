657e4ea6d72efc47ac428d8497f0b1bc86fb45db
Tamas Gal <tgal@km3net.de>
2017-11-05 13:57:30 +0100

    Add qsub, which can submit jobs from python

--- km3pipe/shell.py
+++ km3pipe/shell.py
@@ -11,6 +11,7 @@ from __future__ import division, absolute_import, print_function
 
 import os
 
+from .dev import lstrip
 from .logger import logging
 
 __author__ = "Tamas Gal"
@@ -22,6 +23,59 @@ __email__ = "tgal@km3net.de"
 __status__ = "Development"
 
 log = logging.getLogger(__name__)  # pylint: disable=C0103
+
+
+JOB_TEMPLATE = lstrip("""
+    ########################################################
+    #$ -j y
+    #$ -M {email}
+    ## Send mail at: start (b), completion (e), never (n)
+    #$ -m {send_mail}
+    #$ -o {log_path}/{job_name}.log
+    #$ -P P_{group}
+    # 
+    ## Walltime (HH:MM:SS)
+    #$ -l ct={walltime}
+    ## Memory (Units: G, M, K, B; Min 64M)
+    #$ -l vmem={vmem}
+    ## Local scratch diskspace (at TMPDIR)
+    #$ -l fsize={fsize}
+    #
+    ## Extra Resources
+    #$ -l irods={irods:d}
+    #$ -l sps={sps:d}
+    #$ -l hpss={hpss:d}
+    #$ -l xrootd={xrootd:d}
+    #$ -l dcache={dcache:d}
+    #$ -l oracle={oracle:d}
+    ########################################################
+
+    echo "========================================================"
+    echo "Job started on" $(date)
+    echo "========================================================"
+
+    {script}
+
+    echo "========================================================"
+    echo "Job exited on" $(date)
+    echo "========================================================"
+""")
+
+
+def qsub(script, job_name, log_path=os.getcwd(), group='km3net',
+         walltime='00:10:00', vmem='1G', fsize='10G',
+         email=os.environ['USER']+'@km3net.de', send_mail='n',
+         irods=False, sps=True, hpss=False, xrootd=False,
+         dcache=False, oracle=False):
+    """Submit a job via qsub."""
+    job_string = JOB_TEMPLATE.format(
+            script=script, email=email, send_mail=send_mail, log_path=log_path,
+            job_name=job_name, group=group, walltime=walltime, vmem=vmem,
+            fsize=fsize, irods=irods, sps=sps, hpss=hpss, xrootd=xrootd,
+            dcache=dcache, oracle=oracle)
+    env = os.environ.copy()
+    p = subprocess.Popen('qsub -V', stdin=subprocess.PIPE, env=env)
+    p.communicate(input=bytes(job_string))
 
 
 def get_jpp_env(jpp_dir):
