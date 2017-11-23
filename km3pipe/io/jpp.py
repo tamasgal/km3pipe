80e4bbcc3e4d7a9d43a85b898025289b76f43c0f
Moritz Lotze <lotzemoritz@gmail.com>
2017-11-23 20:04:39 +0100

    fix attribution

--- km3pipe/io/jpp.py
+++ km3pipe/io/jpp.py
@@ -19,7 +19,7 @@ log = logging.getLogger(__name__)  # pylint: disable=C0103
 
 __author__ = "Tamas Gal"
 __copyright__ = "Copyright 2016, Tamas Gal and the KM3NeT collaboration."
-__credits__ = ["Thomas Heid"]
+__credits__ = ["Thomas Heid", "Giuliano Maggi", "Moritz Lotze"]
 __license__ = "MIT"
 __maintainer__ = "Tamas Gal"
 __email__ = "tgal@km3net.de"
