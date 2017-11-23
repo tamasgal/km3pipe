d992d75eb7808f305268a5ac8abec6c6b7f46380
Moritz Lotze <mlotze@km3net.de>
2017-11-23 19:57:33 +0100

    add ``kp.io.jpp.Fitpump`` example to gallery

---
+++ examples/highlevel_analysis/README.txt
@@ -0,0 +1,8 @@
+.. _highlevel_analysis:
+
+Highlevel Analysis
+------------------
+
+Scripts to show how to access and analyse highlevel data,
+like comparing Reconstructions and true neutrino data.
+
---
+++ examples/highlevel_analysis/test_kp_jfit.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+Usage:
+    test_kp_jfit.py FILENAME
+"""
+
+from docopt import docopt
+
+from km3pipe.io.hdf5 import HDF5Sink
+from km3pipe.io.jpp import FitPump
+from km3pipe import Pipeline
+
+
+def print_fits(blob):
+    fits = blob['JFit']
+    print(fits[:10])
+
+
+args = docopt(__doc__)
+fname = args['FILENAME']
+
+pipe = Pipeline()
+pipe.attach(FitPump, filename=fname)
+pipe.attach(print_fits)
+pipe.attach(HDF5Sink, filename=fname + '.h5')
+pipe.drain(1)
