b0aa8d71935f07e0ae7d885924f762ca2ac1f6ac
Moritz Lotze <lotzemoritz@gmail.com>
2017-12-10 16:35:28 +0100

    make sure the array is an array

--- km3pipe/io/hdf5.py
+++ km3pipe/io/hdf5.py
@@ -124,6 +124,7 @@ class HDF5Sink(Module):
             return data.serialise()
         except AttributeError:
             pass
+        data = np.asarray(data)
         return data
 
     def _write_array(self, where, arr, datatype, title=''):
@@ -184,7 +185,6 @@ class HDF5Sink(Module):
         d["index"] += n_items
 
     def process(self, blob):
-
         if not self._header_written and "Header" in blob \
                 and blob["Header"] is not None:
             header = self.h5file.create_group('/', 'header', 'Header')
