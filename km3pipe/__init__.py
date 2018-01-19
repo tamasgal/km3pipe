c18bd2516f304b54bfee0c673544dedc4d82a70f
Tamas Gal <tgal@km3net.de>
2018-01-19 13:47:08 +0100

    Import all submodules. wtf

--- km3pipe/__init__.py
+++ km3pipe/__init__.py
@@ -16,8 +16,27 @@ from .__version__ import version, VERSION_INFO  # noqa
 
 if not __KM3PIPE_SETUP__:
     from .core import (Pipeline, Module, Pump, Blob, Run, Geometry)  # noqa
+    import km3pipe.calib  # noqa
+    import km3pipe.cmd  # noqa
+    import km3pipe.common  # noqa
+    import km3pipe.config  # noqa
+    import km3pipe.constants  # noqa
+    import km3pipe.controlhost  # noqa
+    import km3pipe.core  # noqa
+    import km3pipe.dataclasses  # noqa
     import km3pipe.db  # noqa
+    import km3pipe.hardware  # noqa
     import km3pipe.io  # noqa
+    import km3pipe.logger  # noqa
+    import km3pipe.math  # noqa
+    import km3pipe.mc  # noqa
+    import km3pipe.shell  # noqa
+    import km3pipe.srv  # noqa
+    import km3pipe.style  # noqa
+    import km3pipe.sys  # noqa
+    import km3pipe.testing  # noqa
+    import km3pipe.time  # noqa
+    import km3pipe.tools  # noqa
 
 
 __author__ = "Tamas Gal and Moritz Lotze"
