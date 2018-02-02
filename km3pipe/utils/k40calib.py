7797aea838aff0d2226a86204d9c28b2727fef50
Tamas Gal <tgal@km3net.de>
2018-02-02 12:02:53 +0100

    Fix stream...

--- km3pipe/utils/k40calib.py
+++ km3pipe/utils/k40calib.py
@@ -46,7 +46,7 @@ __status__ = "Development"
 
 def k40calib(filename, tmax, ctmin, stream, filter_hrv, det_id, calib_filename):
     pipe = kp.Pipeline()
-    pipe.attach(kp.io.jpp.TimeslicePump, filename=filename)
+    pipe.attach(kp.io.jpp.TimeslicePump, filename=filename, stream=stream)
     pipe.attach(StatusBar, every=5000)
     pipe.attach(MemoryObserver, every=10000)
     pipe.attach(k40.HRVFIFOTimesliceFilter,
