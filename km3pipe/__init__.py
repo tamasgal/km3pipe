77e448873abede6354d956bb059b7b0fd612a7ac
Moritz Lotze <mlotze@km3net.de>
2018-02-21 11:52:43 +0100

    try sorting imports in init

--- km3pipe/__init__.py
+++ km3pipe/__init__.py
@@ -16,14 +16,14 @@ from .__version__ import version, VERSION_INFO  # noqa
 
 if not __KM3PIPE_SETUP__:
     from .core import (Pipeline, Module, Pump, Blob, Run, Geometry)  # noqa
+    import km3pipe.core  # noqa
+    import km3pipe.dataclasses  # noqa
     import km3pipe.calib  # noqa
     import km3pipe.cmd  # noqa
     import km3pipe.common  # noqa
     import km3pipe.config  # noqa
     import km3pipe.constants  # noqa
     import km3pipe.controlhost  # noqa
-    import km3pipe.core  # noqa
-    import km3pipe.dataclasses  # noqa
     import km3pipe.db  # noqa
     import km3pipe.hardware  # noqa
     import km3pipe.io  # noqa
