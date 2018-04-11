2875f70822830f1c31c8ee76e2054a715d9a34f2
Moritz Lotze <lotzemoritz@gmail.com>
2018-04-11 12:46:08 +0200

    keep .evt dataclasses isolated

--- km3pipe/io/evt.py
+++ km3pipe/io/evt.py
@@ -14,7 +14,7 @@ from collections import namedtuple, defaultdict
 from km3pipe.core import Pump, Blob
 from km3pipe.logger import logging
 
-from km3pipe.dataclasses import Table, Point, Direction
+from km3pipe.dataclasses import Table
 from km3pipe.tools import unpack_nfirst
 from km3pipe.mc import pdg2name, geant2pdg
 from km3pipe.sys import ignored
@@ -35,6 +35,41 @@ def try_decode_string(foo):
         return foo.decode('ascii')
     except AttributeError:
         return foo
+
+
+class Point(np.ndarray):
+    """Represents a point in a 3D space"""
+    def __new__(cls, input_array=(np.nan, np.nan, np.nan)):
+        """Add x, y and z to the ndarray"""
+        obj = np.asarray(input_array).view(cls)
+        return obj
+
+    @property
+    def x(self):
+        return self[0]
+
+    @x.setter
+    def x(self, value):
+        self[0] = value
+
+    @property
+    def y(self):
+        return self[1]
+
+    @y.setter
+    def y(self, value):
+        self[1] = value
+
+    @property
+    def z(self):
+        return self[2]
+
+    @z.setter
+    def z(self, value):
+        self[2] = value
+
+
+Position = Direction = Point  # Backwards compatibility
 
 
 class EvtPump(Pump):  # pylint: disable:R0902
