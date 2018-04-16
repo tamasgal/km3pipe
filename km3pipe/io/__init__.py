180cd45f05d5dd3cd26ee223c3a8a7528d9bfa64
Moritz Lotze <lotzemoritz@gmail.com>
2018-04-16 15:44:43 +0200

    test the h5 pumps, too

--- km3pipe/io/__init__.py
+++ km3pipe/io/__init__.py
@@ -17,9 +17,9 @@ from .evt import EvtPump  # noqa
 #from .aanet import AanetPump  # noqa
 #from .jpp import EventPump  # noqa
 #from .ch import CHPump  # noqa
-#from .hdf5 import HDF5Pump  # noqa
-#from .hdf5 import HDF5Sink  # noqa
-#from .hdf5 import HDF5MetaData  # noqa
+from .hdf5 import HDF5Pump  # noqa
+from .hdf5 import HDF5Sink  # noqa
+from .hdf5 import HDF5MetaData  # noqa
 #from .pickle import PicklePump  # noqa
 from .pandas import (H5Chain, df_to_h5, map2df,  # noqa
                      read_group, write_table,)  # noqa
