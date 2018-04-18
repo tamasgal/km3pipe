13cd6091a4841c5328923173f8b98a58078e6d70
Tamas Gal <tgal@km3net.de>
2018-04-18 21:41:32 +0200

    Cleanup old stuff

--- km3pipe/io/evt.py
+++ km3pipe/io/evt.py
@@ -6,17 +6,8 @@ Pumps for the EVT simulation dataformat.
 """
 import sys
 
-from collections import namedtuple, defaultdict
-
-import numpy as np
-
 from km3pipe.core import Pump, Blob
 from km3pipe.logger import logging
-
-from km3pipe.dataclasses import Table
-from km3pipe.tools import unpack_nfirst
-from km3pipe.mc import pdg2name, geant2pdg
-from km3pipe.sys import ignored
 
 log = logging.getLogger(__name__)  # pylint: disable=C0103
 
@@ -29,11 +20,12 @@ __email__ = "tgal@km3net.de"
 __status__ = "Development"
 
 
-def try_decode_string(foo):
+def try_decode_string(text):
+    """Decode string to ASCII if possible"""
     try:
-        return foo.decode('ascii')
+        return text.decode('ascii')
     except AttributeError:
-        return foo
+        return text
 
 
 class EvtPump(Pump):  # pylint: disable:R0902
@@ -115,7 +107,6 @@ class EvtPump(Pump):  # pylint: disable:R0902
         if self.n_digits is not None:
             file_index = file_index.zfill(self.n_digits)
         return file_index
-
 
     def prepare_blobs(self):
         """Populate the blobs"""
@@ -275,158 +266,3 @@ class EvtPump(Pump):  # pylint: disable:R0902
     def finish(self):
         """Clean everything up"""
         self.blob_file.close()
-
-
-class Track(object):
-    """Bass class for particle or shower tracks"""
-#    def __init__(self, id, x, y, z, dx, dy, dz, E=None, t=0, *args):
-
-    def __init__(self, data):
-        id, x, y, z, dx, dy, dz, E, t, args = unpack_nfirst(data, 9)
-        self.id = int(id)
-        self.pos = (x, y, z)
-        self.dir = (dx, dy, dz)
-        self.E = E
-        self.time = t
-        self.args = args
-
-    def __repr__(self):
-        text = "Track:\n"
-        text += " id: {0}\n".format(self.id)
-        text += " pos: {0}\n".format(self.pos)
-        text += " dir: {0}\n".format(self.dir)
-        text += " energy: {0} GeV\n".format(self.E)
-        text += " time: {0} ns\n".format(self.time)
-        return text
-
-
-class TrackIn(Track):
-    """Representation of a track_in entry in an EVT file"""
-
-    def __init__(self, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
-        try:
-            self.particle_type = int(self.args[0])
-            self.charmed = bool(self.args[1])
-            self.mother = int(self.args[2])
-            self.grandmother = int(self.args[3])
-            self.length = 0
-        except IndexError:
-            self.particle_type = geant2pdg(int(self.args[0]))
-            try:
-                self.length = self.args[1]
-            except IndexError:
-                self.length = 0
-
-    def __repr__(self):
-        text = super(self.__class__, self).__repr__()
-        text += " length: {0} [m]\n".format(self.length)
-        try:
-            text += " type: {0} [Corsika]\n".format(self.particle_type)
-            text += " charmed: {0}\n".format(self.charmed)
-            text += " mother: {0} [Corsika]\n".format(self.mother)
-            text += " grandmother: {0} [Corsika]\n".format(self.grandmother)
-        except AttributeError:
-            text += " type: {0} '{1}' [PDG]\n"  \
-                    .format(self.particle_type,
-                            pdg2name(self.particle_type))
-            pass
-
-        return text
-
-
-class TrackCorsika(Track):
-    """Representation of a track in a corsika output file"""
-
-    def __init__(self, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
-        self.particle_type = int(self.args[0])
-        try:
-            self.charmed = bool(self.args[1])
-            self.mother = int(self.args[2])
-            self.grandmother = int(self.args[3])
-        except IndexError:
-            pass
-
-    def __repr__(self):
-        text = super(self.__class__, self).__repr__()
-        text += " type: {0} [Corsika]\n".format(self.particle_type)
-        try:
-            text += " charmed: {0}\n".format(self.charmed)
-            text += " mother: {0} [Corsika]\n".format(self.mother)
-            text += " grandmother: {0} [Corsika]\n".format(self.grandmother)
-        except AttributeError:
-            pass
-        return text
-
-
-class TrackFit(Track):
-    """Representation of a track_fit entry in an EVT file"""
-
-    def __init__(self, *args, **kwargs):
-        super(self.__class__, self).__init__(*args, **kwargs)
-        self.speed = self.args[0]
-        self.ts = self.args[1]
-        self.te = self.args[2]
-        self.con1 = self.args[3]
-        self.con2 = self.args[4]
-
-    def __repr__(self):
-        text = super(self.__class__, self).__repr__()
-        text += " speed: {0} [m/ns]\n".format(self.speed)
-        text += " ts: {0} [ns]\n".format(self.ts)
-        text += " te: {0} [ns]\n".format(self.te)
-        text += " con1: {0}\n".format(self.con1)
-        text += " con2: {0}\n".format(self.con2)
-        return text
-
-
-class Neutrino(object):  # pylint: disable:R0902
-    """Representation of a neutrino entry in an EVT file"""
-
-    def __init__(self, data):
-        id, x, y, z, dx, dy, dz, E, t, Bx, By, \
-            ichan, particle_type, channel, args = unpack_nfirst(data, 14)
-        self.id = id
-        # z correctio due to gen/km3 (ZED -> sea level shift)
-        # http://wiki.km3net.physik.uni-erlangen.de/index.php/Simulations
-        self.pos = (x, y, z)
-        self.dir = (dx, dy, dz)
-        self.E = E
-        self.time = t
-        self.Bx = Bx
-        self.By = By
-        self.ichan = ichan
-        self.particle_type = particle_type
-        self.channel = channel
-
-    def __str__(self):
-        text = "Neutrino: "
-        text += pdg2name(self.particle_type)
-        if self.E >= 1000000:
-            text += ", {0:.3} PeV".format(self.E / 1000000)
-        elif self.E >= 1000:
-            text += ", {0:.3} TeV".format(self.E / 1000)
-        else:
-            text += ", {0:.3} GeV".format(float(self.E))
-        text += ', CC' if int(self.channel) == 2 else ', NC'
-        return text
-
-
-# The hit entry in an EVT file
-EvtHit = namedtuple('EvtHit',
-                    'id pmt_id pe time type n_photons track_in c_time')
-EvtHit.__new__.__defaults__ = (None, None, None, None, None, None, None, None)
-
-
-# The hit_raw entry in an EVT file
-def __add_raw_hit__(self, other):
-    """Add two hits by adding the ToT and preserve time and pmt_id
-    of the earlier one."""
-    first = self if self.time <= other.time else other
-    return EvtRawHit(first.id, first.pmt_id, self.tot + other.tot, first.time)
-
-
-EvtRawHit = namedtuple('EvtRawHit', 'id pmt_id tot time')
-EvtRawHit.__new__.__defaults__ = (None, None, None, None)
-EvtRawHit.__add__ = __add_raw_hit__
