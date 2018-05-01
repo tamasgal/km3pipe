24af9ca5e94b5dd92202b1b662bd15d059aed2bc
Tamas Gal <tgal@km3net.de>
2018-05-01 21:17:34 +0200

    Cleanup

--- km3pipe/io/__init__.py
+++ km3pipe/io/__init__.py
@@ -3,7 +3,6 @@
 A collection of io for different kinds of data formats.
 
 """
-
 import os.path
 
 import numpy as np
@@ -18,8 +17,6 @@ from .hdf5 import HDF5Pump  # noqa
 from .hdf5 import HDF5Sink  # noqa
 from .hdf5 import HDF5MetaData  # noqa
 from .pickle import PicklePump  # noqa
-from .pandas import (H5Chain, df_to_h5, map2df,  # noqa
-                     read_group, write_table,)  # noqa
 
 from km3pipe.logger import logging
 
@@ -54,10 +51,10 @@ def GenericPump(filenames, use_jppy=False, name="GenericPump", **kwargs):
 
     io = {
         '.evt': EvtPump,
-         '.h5': HDF5Pump,
-         '.root': EventPump if use_jppy else AanetPump,
-         '.dat': DAQPump,
-         '.dqd': CLBPump,
+        '.h5': HDF5Pump,
+        '.root': EventPump if use_jppy else AanetPump,
+        '.dat': DAQPump,
+        '.dqd': CLBPump,
     }
 
     if extension not in io:
