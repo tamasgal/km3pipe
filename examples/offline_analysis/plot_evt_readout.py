fb63ff9af8120a0b18c46009552bb33212436a82
Moritz Lotze <lotzemoritz@gmail.com>
2018-05-07 13:22:49 +0200

    DOC update evt_readout example

--- examples/offline_analysis/plot_evt_readout.py
+++ examples/offline_analysis/plot_evt_readout.py
@@ -9,37 +9,51 @@ This example shows how to read and parse EVT files, which are used in our
 Monte Carlo productions.
 
 """
-# Author: Tamas Gal <tgal@km3net.de>
+# Author: Tamas Gal <tgal@km3net.de>, Moritz Lotze >mlotze@km3net.de>
 # License: BSD-3
+import matplotlib.pyplot as plt
 import numpy as np
-import km3pipe as kp
-from km3pipe.dataclasses import Table
+
 from km3modules.common import StatusBar
+from km3pipe import Module, Pipeline
+from km3pipe.dataclasses import Table
+from km3pipe.calib import Calibration
+from km3pipe.io import EvtPump
+from km3pipe.math import pld3
 import km3pipe.style
+
 km3pipe.style.use("km3pipe")
 
 
 filename = "../data/numu_cc.evt"
 detx = "../data/km3net_jul13_90m_r1494_corrected.detx"
 
-cal = kp.calib.Calibration(filename=detx)
+cal = Calibration(filename=detx)
 
 
-class VertexHitDistanceCalculator(kp.Module):
+class VertexHitDistanceCalculator(Module):
     """Calculate vertex-hit-distances"""
+    def configure(self):
+        self.distances = []
 
     def process(self, blob):
-        print(blob.keys())
-        print(blob['raw_header']['seed'])
-        # tracks = blob['McTracks']
-        # muons = tracks[tracks.type == 5]
-        # muon = Table(muons[np.argmax(muons.energy)])
-        # print(muon)
+        tracks = blob['TrackIns']
+        muons = tracks[tracks.type == 5]
+        muon = Table(muons[np.argmax(muons.energy)])
+        hits = blob['Hits']
+        hits = cal.apply(hits)
+        dist = pld3(hits.pos, muon.pos, muon.dir)
+        self.distances.append(dist)
         return blob
 
+    def finish(self):
+        dist_flat = np.concatenate(self.distances)
+        plt.hist(dist_flat)
+        plt.savefig('dists.pdf')
 
-pipe = kp.Pipeline()
-pipe.attach(kp.io.EvtPump, filename=filename, parsers='auto')
+
+pipe = Pipeline()
+pipe.attach(EvtPump, filename=filename, parsers=['km3'])
 pipe.attach(StatusBar, every=100)
 pipe.attach(VertexHitDistanceCalculator)
-pipe.drain(2)
+pipe.drain(5)
