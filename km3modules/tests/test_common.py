78a19087318ef83b086db94ca698726002815e94
Tamas Gal <tgal@km3net.de>
2018-05-12 15:50:21 +0200

    Add some tests for common km3modules

--- km3modules/tests/test_common.py
+++ km3modules/tests/test_common.py
@@ -2,8 +2,10 @@
 # pylint: disable=locally-disabled,C0111,R0904,C0103
 
 import km3pipe as kp
-from km3modules.common import Siphon, Delete, Keep, Wrap
+from km3modules.common import (Siphon, Delete, Keep, Wrap, Dump, StatusBar,
+                               TickTock, MemoryObserver)
 from km3pipe.testing import TestCase, MagicMock
+from km3pipe.tools import istype
 
 __author__ = "Tamas Gal"
 __copyright__ = "Copyright 2016, Tamas Gal and the KM3NeT collaboration."
@@ -12,6 +14,17 @@ __license__ = "MIT"
 __maintainer__ = "Tamas Gal"
 __email__ = "tgal@km3net.de"
 __status__ = "Development"
+
+
+class InfinitePump(kp.Pump):
+    """A pump which just infinetly spits out indexed blobs"""
+    def configure(self):
+        self.i = 0
+
+    def process(self, blob):
+        self.i += 1
+        blob['i'] = self.i
+        return blob
 
 
 class TestKeep(TestCase):
@@ -65,17 +78,6 @@ class TestDelete(TestCase):
 
 
 class TestSiphon(TestCase):
-    def setUp(self):
-        class APump(kp.Pump):
-            def configure(self):
-                self.i = 0
-
-            def process(self, blob):
-                self.i += 1
-                blob['i'] = self.i
-                return blob
-        self.Pump = APump
-
     def test_siphon(self):
         class Observer(kp.Module):
             def configure(self):
@@ -89,7 +91,7 @@ class TestSiphon(TestCase):
                 assert self.mock.call_count == 7
 
         pipe = kp.Pipeline()
-        pipe.attach(self.Pump)
+        pipe.attach(InfinitePump)
         pipe.attach(Siphon, volume=10)
         pipe.attach(Observer)
         pipe.drain(17)
@@ -107,7 +109,7 @@ class TestSiphon(TestCase):
                 assert self.mock.call_count == 1
 
         pipe = kp.Pipeline()
-        pipe.attach(self.Pump)
+        pipe.attach(InfinitePump)
         pipe.attach(Siphon, volume=10, flush=True)
         pipe.attach(Observer)
         pipe.drain(21)
@@ -125,7 +127,89 @@ class TestSiphon(TestCase):
                 assert self.mock.call_count == 2
 
         pipe = kp.Pipeline()
-        pipe.attach(self.Pump)
+        pipe.attach(InfinitePump)
         pipe.attach(Siphon, volume=10, flush=True)
         pipe.attach(Observer)
         pipe.drain(22)
+
+
+class TestWrap(TestCase):
+    def test_wrap_a_single_value(self):
+        def add_a_keyval_dict(blob):
+            blob['a'] = {'b': 1, 'c': 2.3}
+            return blob
+
+        def check_wrapped_key(blob):
+            assert 'Generic Table' == blob['a'].name
+            assert 1 == len(blob['a'])
+            assert 1 == blob['a'].b
+            assert 2.3 == blob['a'].c
+            return blob
+
+        pipe = kp.Pipeline()
+        pipe.attach(InfinitePump)
+        pipe.attach(add_a_keyval_dict)
+        pipe.attach(Wrap, key='a')
+        pipe.attach(check_wrapped_key)
+        pipe.drain(3)
+
+    def test_wrap_multiple_values(self):
+        def add_a_keyval_dict(blob):
+            blob['a'] = {'b': 1, 'c': 2.3}
+            blob['d'] = {'e': [4, 5], 'f': [6.7, 8.9]}
+            return blob
+
+        def check_wrapped_key(blob):
+            assert 'Generic Table' == blob['a'].name
+            assert 1 == len(blob['a'])
+            assert 1 == blob['a'].b
+            assert 2.3 == blob['a'].c
+            assert 'Generic Table' == blob['d'].name
+            assert 2 == len(blob['d'])
+            assert 4 == blob['d'].e[0]
+            assert 8.9 == blob['d'].f[1]
+            return blob
+
+        pipe = kp.Pipeline()
+        pipe.attach(InfinitePump)
+        pipe.attach(add_a_keyval_dict)
+        pipe.attach(Wrap, keys=['a', 'd'])
+        pipe.attach(check_wrapped_key)
+        pipe.drain(3)
+
+
+class TestDump(TestCase):
+    def test_dump(self):
+        def add_something(blob):
+            blob['a'] = 1
+            return blob
+
+        pipe = kp.Pipeline()
+        pipe.attach(InfinitePump)
+        pipe.attach(add_something)
+        pipe.attach(Dump)
+        pipe.drain(3)
+
+
+class TestStatusbar(TestCase):
+    def test_statusbar(self):
+        pipe = kp.Pipeline()
+        pipe.attach(InfinitePump)
+        pipe.attach(StatusBar, every=2)
+        pipe.drain(5)
+
+
+class TestTickTock(TestCase):
+    def test_ticktock(self):
+        pipe = kp.Pipeline()
+        pipe.attach(InfinitePump)
+        pipe.attach(TickTock)
+        pipe.drain(5)
+
+
+class TestMemoryObserver(TestCase):
+    def test_memory_observer(self):
+        pipe = kp.Pipeline()
+        pipe.attach(InfinitePump)
+        pipe.attach(MemoryObserver)
+        pipe.drain(5)
