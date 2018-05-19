Fixes DOM rotation by yaw. Ref #41

km3pipe/hardware.py
@@ -236,15 +236,17 @@ class Detector(object):
         self.pmts.pos_z += vector[2]
         self.reset_caches()
 
-    def rotate_dom_by_yaw(self, dom_id, heading):
+    def rotate_dom_by_yaw(self, dom_id, heading, centre_point=None):
         """Rotate a DOM by a given (yaw) heading."""
         pmts = self.pmts[self.pmts.dom_id == dom_id]
-        dom_centre = self.dom_positions[dom_id]
+        if centre_point is None:
+            centre_point = self.dom_positions[dom_id]
+            
 
         for pmt in pmts:
             pmt_pos = np.array([pmt.pos_x, pmt.pos_y, pmt.pos_z])
             pmt_dir = np.array([pmt.dir_x, pmt.dir_y, pmt.dir_z])
-            pmt_radius = np.linalg.norm(dom_centre - pmt_pos)
+            pmt_radius = np.linalg.norm(centre_point - pmt_pos)
             index = self._pmt_index_by_pmt_id[pmt.pmt_id]
             pmt_ref = self.pmts[index]
 

km3pipe/tests/test_hardware.py
@@ -61,6 +61,21 @@ EXAMPLE_DETX_MIXED_IDS = StringIO("\n".join((
     " 63 3.4 3.5 3.6  0.1 -1.2  0.3 80",
     " 61 3.7 3.8 3.9  0.1  0.2 -1.3 90",)))
 
+EXAMPLE_DETX_RADIAL = StringIO("\n".join((
+    "1 3",
+    "1 1 1 4",
+    " 1 1 0 0 1 0 0 10",
+    " 2 0 1 0 0 1 0 20",
+    " 3 -1 0 0 -1 0 0 30",
+    " 4 0 -1 0 0 -1 0 40",
+    "2 1 2 2",
+    " 5 0 0 1 0 0 1 50",
+    " 6 0 0 -1 0 0 -1 60",
+    "3 1 3 2",
+    " 7 1 2 3 1 2 3 70",
+    " 8 -3 -2 -1 -3 -2 -1 80",
+    )))
+
 
 class TestDetector(TestCase):
 
@@ -244,38 +259,27 @@ class TestDetector(TestCase):
             assert np.allclose(orig_dom_pos[dom_id] + t_vec, pos)
 
     def test_rotate_dom_by_yaw(self):
+        det = Detector()
+        det._det_file = EXAMPLE_DETX_RADIAL
+        det._parse_doms()
+        # here, only one PMT is checked
         dom_id = 1
         heading = 23
         channel_id = 0
-        det = self.det
-        det._parse_doms()
         pmt_dir = det.pmts[det.pmts.dom_id == dom_id].dir[channel_id].copy()
         pmt_dir_rot = qrot_yaw(pmt_dir, heading)
         det.rotate_dom_by_yaw(dom_id, heading)
         assert np.allclose(pmt_dir_rot,
                            det.pmts[det.pmts.dom_id == dom_id].dir[channel_id])
-        assert np.allclose([1.10493588, 0.96340793, 1.3],
-                           det.pmts[det.pmts.dom_id == dom_id].pos[channel_id])
-
-    def test_rotate_dom_by_yaw_2(self):
-        dom_id = 2
-        heading = -42 
-        channel_id = 2
-        det = self.det
-        det._parse_doms()
-        pmt_dir = det.pmts[det.pmts.dom_id == dom_id].dir[channel_id].copy()
-        pmt_dir_rot = qrot_yaw(pmt_dir, heading)
-        det.rotate_dom_by_yaw(dom_id, heading)
-        assert np.allclose(pmt_dir_rot,
-                           det.pmts[det.pmts.dom_id == dom_id].dir[channel_id])
-        assert np.allclose([2.76202094, 2.73216155, 2.9],
+        assert np.allclose([0.92050485, 0.39073113, 0],
                            det.pmts[det.pmts.dom_id == dom_id].pos[channel_id])
 
     def test_rotate_dom_set_by_step_by_360_degrees(self):
+        det = Detector()
+        det._det_file = EXAMPLE_DETX_RADIAL
+        det._parse_doms()
         dom_id = 1
         channel_id = 0
-        det = self.det
-        det._parse_doms()
         pmt_dir = det.pmts[det.pmts.dom_id == dom_id].dir[channel_id].copy()
         pmt_pos = det.pmts[det.pmts.dom_id == dom_id].pos[channel_id].copy()
         for i in range(360):