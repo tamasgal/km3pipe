f95a3208eb393383011de0b9d843b0a299fdda6d
Moritz Lotze <lotzemoritz@gmail.com>
2018-05-31 17:00:45 +0200

    remove tests for old nonsense

--- km3pipe/io/tests/test_aanet.py
+++ km3pipe/io/tests/test_aanet.py
@@ -1,6 +1,6 @@
 # Filename: test_aanet.py
 # pylint: disable=locally-disabled,C0111,R0904,C0301,C0103,W0212
-from km3pipe.testing import TestCase, patch, Mock
+from km3pipe.testing import TestCase, patch, Mock, skip
 from km3pipe.io.aanet import AanetPump
 
 import sys
@@ -21,22 +21,8 @@ class TestAanetPump(TestCase):
         with self.assertRaises(ValueError):
             AanetPump()
 
+    @skip
     def test_init_with_filename(self):
         filename = 'a'
         p = AanetPump(filename=filename)
         assert filename in p.filenames
-
-    @patch("ROOT.gSystem")
-    def test_init_with_custom_aanet_lib(self, root_gsystem_mock):
-        filename = 'a'
-        custom_aalib = 'an_aalib'
-        p = AanetPump(filename=filename, aa_lib=custom_aalib)
-        assert filename in p.filenames
-        root_gsystem_mock.Load.assert_called_once_with(custom_aalib)
-
-    def test_init_with_indexed_filenames(self):
-        filename = 'a[index]b'
-        indices = [1, 2, 3]
-        p = AanetPump(filename=filename, indices=indices)
-        for index in indices:
-            assert "a"+str(index)+"b" in p.filenames
