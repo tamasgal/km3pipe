99abe6ac616e94a6a2aa49d100bfc873a4576968
Piotr Kalaczy?ski <pkalaczynski@km3net.de>
2018-10-28 07:28:42 +0100

    Update test_evt.py

--- km3pipe/io/tests/test_evt.py
+++ km3pipe/io/tests/test_evt.py
@@ -288,10 +288,10 @@ class TestCorsika(TestCase):
 
 class TestPropa(TestCase):
     def setUp(self):
-        self.fname = join(TEST_DATA_DIR, 'test_file.evt')
+        self.fname = join(TEST_DATA_DIR, 'example_corant_propa.evt')
         self.fnames = []
         for i in [0,1]:
-            self.fnames.append(join(TEST_DATA_DIR, 'test_file.evt'))
+            self.fnames.append(join(TEST_DATA_DIR, 'example_corant_propa.evt'))
 
     def test_pipe(self):
         pump = EvtPump(filename=self.fname, parsers=['propa'])
