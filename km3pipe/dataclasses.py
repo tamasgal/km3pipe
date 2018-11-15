3f215d1793a1baa887e8bc6f5037b1ebd5f30841
Johannes Schumann <johannes.schumann@fau.de>
2018-11-15 12:03:20 +0100

    Added ability to add tables with different columns

--- km3pipe/dataclasses.py
+++ km3pipe/dataclasses.py
@@ -405,12 +405,10 @@ class Table(np.recarray):
         return cls(rec, **kwargs)
 
     def __add__(self, other):
-        if not self.dtype == other.dtype:
-            raise KeyError("dtypes of added tables do not match")
         import pandas as pd
         df1 = pd.DataFrame(self)
         df2 = pd.DataFrame(other)
-        df_cc = pd.concat([df1, df2], ignore_index=True)
+        df_cc = pd.concat([df1, df2], ignore_index=True,sort=False)
         ret_tbl = Table(df_cc.to_dict('list'))
         return ret_tbl
 
