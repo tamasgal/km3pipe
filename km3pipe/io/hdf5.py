0b2520348ff6e2fd40bfce6a6d4738dfb8f309f4
Tamas Gal <tgal@km3net.de>
2018-12-15 20:36:53 +0100

    Fix writing of split columns in same group

--- km3pipe/io/hdf5.py
+++ km3pipe/io/hdf5.py
@@ -301,7 +301,8 @@ class HDF5Sink(Module):
         f = self.h5file
         loc, group_name = os.path.split(where)
         if where not in f:
-            group = f.create_group(loc, group_name, title)
+            print(where)
+            group = f.create_group(loc, group_name, title, createparents=True)
             group._v_attrs.datatype = title
         else:
             group = f.get_node(where)
