b93eb44ffa8ce539a13d8eaaa40d1f3bb00663e8
Tamas Gal <tgal@km3net.de>
2019-01-18 10:23:15 +0100

    Add more tests for TMCHRepump

--- km3pipe/io/tests/test_daq.py
+++ km3pipe/io/tests/test_daq.py
@@ -6,6 +6,8 @@ Tests for KM3NeT binary formats readout.
 """
 from os.path import dirname, join
 
+import numpy as np
+
 from km3pipe.testing import TestCase
 from km3pipe.io.daq import (
     DAQPump, DAQPreamble, DAQHeader, DAQSummaryslice, DMMonitor, TMCHRepump
@@ -68,8 +70,44 @@ class TestDAQPump(TestCase):
 
 
 class TestTMCHRepump(TestCase):
-    def test_init(self):
-        TMCHRepump(filename=IO_MONIT_FILE)
+    def test_reading_version_2(self):
+        repump = TMCHRepump(filename=IO_MONIT_FILE)
+        packets = [p['TMCHData'] for p in repump]
+
+        p1 = packets[0]
+        p2 = packets[5]
+
+        assert 86 == p1.run
+        assert 0 == p1.udp_sequence_number
+        assert 541 == p1.utc_seconds
+        assert 500000000 == p1.nanoseconds
+        assert 806472270 == p1.dom_id
+        assert 2 == p1.version
+        self.assertAlmostEqual(199.05982971191406, p1.yaw)
+        self.assertAlmostEqual(0.5397617816925049, p1.pitch)
+        self.assertAlmostEqual(-0.2243121862411499, p1.roll)
+        self.assertAlmostEqual(32.35, p1.temp)
+        self.assertAlmostEqual(16.77, p1.humidity)
+        assert np.allclose(np.full(31, 0), p1.pmt_rates)
+        assert np.allclose([0.00708725, 0.00213623, -0.86456668], p1.A)
+        assert np.allclose([-0.2621212, 0.02363636, 0.1430303], p1.H)
+        assert np.allclose([-2.87721825, -0.83284622, -0.28969574], p1.G)
+
+        assert 86 == p2.run
+        assert 0 == p2.udp_sequence_number
+        assert 542 == p2.utc_seconds
+        assert 0 == p2.nanoseconds
+        assert 806472270 == p2.dom_id
+        assert 2 == p2.version
+        self.assertAlmostEqual(199.05982971191406, p2.yaw)
+        self.assertAlmostEqual(0.5397617816925049, p2.pitch)
+        self.assertAlmostEqual(-0.2243121862411499, p2.roll)
+        self.assertAlmostEqual(32.35, p2.temp)
+        self.assertAlmostEqual(16.77, p2.humidity)
+        assert np.allclose(np.full(31, 0), p2.pmt_rates)
+        assert np.allclose([0.00708725, 0.00213623, -0.86456668], p2.A)
+        assert np.allclose([-0.2621212, 0.02363636, 0.1430303], p2.H)
+        assert np.allclose([-2.87721825, -0.83284622, -0.28969574], p2.G)
 
 
 class TestDMMonitor(TestCase):
