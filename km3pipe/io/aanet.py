2fa5b21f7fa223a963b755c6b46a5efca5c5e7e3
Piotr Kalaczy?ski <pkalaczynski@km3net.de>
2019-03-07 08:27:37 +0100

    Update aanet.py

--- km3pipe/io/aanet.py
+++ km3pipe/io/aanet.py
@@ -270,7 +270,7 @@ class AanetPump(Pump):
         self._reset_iteration()
         self._reset_state()
         self._open_next_file()
-        self._read_group_info()
+        #self._read_group_info()
         if self.cut_mask_node is not None:
             self._read_cut_mask()
 
