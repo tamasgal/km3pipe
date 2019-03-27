5325acd58218e2757bd7620631d0cc9fb9abc0c9
Tamas Gal <tgal@km3net.de>
2019-03-27 17:08:14 +0100

    Remove debug imports

--- km3pipe/io/tests/test_aanet.py
+++ km3pipe/io/tests/test_aanet.py
@@ -8,9 +8,6 @@ import km3pipe as kp
 from km3pipe.testing import TestCase, patch, Mock, skipif
 from km3pipe.io.aanet import AanetPump, MetaParser
 
-import ROOT
-import aa
-
 try:
     import ROOT
     import aa
