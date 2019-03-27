7af34d9b41da67b4ee94ea8e13c0b22bd7625b42
Tamas Gal <tgal@km3net.de>
2019-03-27 11:46:20 +0100

    Add tests for aanet using sea data

--- km3pipe/io/tests/test_aanet.py
+++ km3pipe/io/tests/test_aanet.py
@@ -4,12 +4,19 @@ from os.path import join, dirname
 
 import numpy as np
 
-from km3pipe.testing import TestCase, patch, Mock, skip
+import km3pipe as kp
+from km3pipe.testing import TestCase, patch, Mock, skipif
 from km3pipe.io.aanet import AanetPump, MetaParser
 
-import sys
-sys.modules['ROOT'] = Mock()
-sys.modules['aa'] = Mock()
+try:
+    import ROOT
+    import aa
+    NO_AANET = False
+except ImportError:
+    import sys
+    sys.modules['ROOT'] = Mock()
+    sys.modules['aa'] = Mock()
+    NO_AANET = True
 
 __author__ = "Tamas Gal"
 __copyright__ = "Copyright 2018, Tamas Gal and the KM3NeT collaboration."
@@ -22,16 +29,46 @@ __status__ = "Development"
 TEST_DATA_DIR = join(dirname(__file__), "../../kp-data/test_data")
 
 
+@skipif(NO_AANET, reason="No aanet environment found.")
 class TestAanetPump(TestCase):
-    def test_init_raises_valueerror_if_no_filename_given(self):
-        with self.assertRaises(TypeError):
-            AanetPump()
+    def setUp(self):
+        self.fname = join(TEST_DATA_DIR, 'sea_data.root')
+        self.pump = AanetPump(filename=self.fname)
 
-    @skip
-    def test_init_with_filename(self):
-        filename = 'a'
-        p = AanetPump(filename=filename)
-        assert filename in p.filenames
+    def test_reading_hits(self):
+        data = {
+            'hit_lengths': [48, 71, 22, 44, 35],
+            'tot_sums': [1252, 1948, 562, 1089, 915],
+            'mean_times': [
+                56379691.1875, 65447948.25352113, 51971419.40909091,
+                38274188.06818182, 73676555.94285715
+            ],
+            'channel_id_sums': [556, 958, 372, 593, 433]
+        }
+
+        for idx, blob in enumerate(self.pump):
+            hits = blob['Hits']
+            assert len(hits) == data['hit_lengths'][idx]
+            self.assertAlmostEqual(data['mean_times'][idx], np.mean(hits.time))
+            assert data['tot_sums'][idx] == np.sum(hits.tot)
+            assert data['channel_id_sums'][idx] == np.sum(hits.channel_id)
+
+    def test_event_info(self):
+        data = {
+            'run_ids': [4143] * 5,
+            'event_ids': [60, 41, 80, 61, 110],
+            'mc_ids': [59, 40, 79, 60, 109],
+            'timestamps': [
+                1551549606, 1551549604, 1551549608, 1551549606, 1551549611
+            ],
+            'nanoseconds': [0, 100000000, 0, 100000000, 0]
+        }
+        for idx, blob in enumerate(self.pump):
+            ei = blob['EventInfo'][0]
+            assert data['run_ids'][idx] == ei.run_id
+            assert data['event_ids'][idx] == ei.event_id
+            assert data['mc_ids'][idx] == ei.mc_id
+            assert data['timestamps'][idx] == ei.timestamp
 
 
 class TestMetaParser(TestCase):
