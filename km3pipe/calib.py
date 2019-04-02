721c46a7ba02f8537153aa9515e7afc634b206b8
Tamas Gal <tgal@km3net.de>
2019-04-02 20:09:56 +0200

    Massive calibration speed-up using numba.typed.Dict

--- km3pipe/calib.py
+++ km3pipe/calib.py
@@ -28,11 +28,20 @@ try:
     import numba as nb
 except (ImportError, OSError):
     HAVE_NUMBA = False
+    jit = lambda f: f
     log.warning(
         "No numba detected, consider `pip install numba` for more speed!"
     )
 else:
-    HAVE_NUMBA = True
+    try:
+        from numba.typed import Dict
+    except ModuleNotFoundError:
+        log.warning("Please update numba (0.43+) to have dictionary support!")
+        HAVE_NUMBA = False
+        jit = lambda f: f
+    else:
+        HAVE_NUMBA = True
+        from numba import jit
 
 
 class Calibration(Module):
@@ -129,9 +138,73 @@ class Calibration(Module):
             # do we ever see McHits here?
             hits = Table.from_template(hits, 'Hits')
         if hasattr(hits, 'dom_id') and hasattr(hits, 'channel_id'):
-            return self._apply_to_hits(hits)
+            dir_x, dir_y, dir_z, du, floor, pos_x, pos_y, pos_z, t0 = _get_calibration_for_hits(
+                hits, self._calib_by_dom_and_channel
+            )
+
+            if hasattr(hits, 'time'):
+                if hits.time.dtype != t0.dtype:
+                    time = hits.time.astype('f4') + t0.astype('f4')
+                    hits = hits.drop_columns(['time'])
+                    hits = hits.append_columns(['time'], [time])
+                else:
+                    hits.time += t0
+
+            hits_data = {}
+            for colname in hits.dtype.names:
+                hits_data[colname] = hits[colname]
+            calib = {
+                'dir_x': dir_x,
+                'dir_y': dir_y,
+                'dir_z': dir_z,
+                'du': du.astype(np.uint8),
+                'floor': du.astype(np.uint8),
+                'pos_x': pos_x,
+                'pos_y': pos_y,
+                'pos_z': pos_z,
+                't0': t0,
+            }
+            hits_data.update(calib)
+            return Table(
+                hits_data,
+                h5loc=hits.h5loc,
+                split_h5=hits.split_h5,
+                name=hits.name
+            )
+
         elif hasattr(hits, 'pmt_id'):
-            return self._apply_to_mchits(hits)
+            dir_x, dir_y, dir_z, du, floor, pos_x, pos_y, pos_z, t0 = _get_calibration_for_mchits(
+                hits, self._calib_by_pmt_id
+            )
+            if hasattr(hits, 'time'):
+                if hits.time.dtype != t0.dtype:
+                    time = hits.time.astype('f4') + t0.astype('f4')
+                    hits = hits.drop_columns(['time'])
+                    hits = hits.append_columns(['time'], [time])
+                else:
+                    hits.time += t0
+
+            hits_data = {}
+            for colname in hits.dtype.names:
+                hits_data[colname] = hits[colname]
+            calib = {
+                'dir_x': dir_x,
+                'dir_y': dir_y,
+                'dir_z': dir_z,
+                'du': du.astype(np.uint8),
+                'floor': du.astype(np.uint8),
+                'pos_x': pos_x,
+                'pos_y': pos_y,
+                'pos_z': pos_z,
+                't0': t0,
+            }
+            hits_data.update(calib)
+            return Table(
+                hits_data,
+                h5loc=hits.h5loc,
+                split_h5=hits.split_h5,
+                name=hits.name
+            )
         else:
             raise TypeError(
                 "Don't know how to apply calibration to '{0}'. "
@@ -139,87 +212,36 @@ class Calibration(Module):
                 "'pmt_id'.".format(hits.name)
             )
 
-    def _apply_to_hits(self, hits):
-        """Append the position, direction and t0 columns and add t0 to time"""
-        n = len(hits)
-        cal = np.empty((n, 9))
-        lookup = self._calib_by_dom_and_channel
-        for i in range(n):
-            calib = lookup[hits['dom_id'][i]][hits['channel_id'][i]]
-            cal[i] = calib
-        dir_x = cal[:, 3]
-        dir_y = cal[:, 4]
-        dir_z = cal[:, 5]
-        du = cal[:, 7]
-        floor = cal[:, 8]
-        pos_x = cal[:, 0]
-        pos_y = cal[:, 1]
-        pos_z = cal[:, 2]
-
-        t0 = cal[:, 6]
-
-        if hasattr(hits, 'time'):
-            if hits.time.dtype != t0.dtype:
-                time = hits.time.astype('f4') + t0.astype('f4')
-                hits = hits.drop_columns(['time'])
-                hits = hits.append_columns(['time'], [time])
-            else:
-                hits.time += t0
-
-        return hits.append_columns([
-            'dir_x', 'dir_y', 'dir_z', 'du', 'floor', 'pos_x', 'pos_y',
-            'pos_z', 't0'
-        ], [dir_x, dir_y, dir_z, du, floor, pos_x, pos_y, pos_z, t0],
-                                   dtypes=[
-                                       '<f8', '<f8', '<f8', '<u2', '<u2',
-                                       '<f8', '<f8', '<f8', '<f8'
-                                   ])
-
-    def _apply_to_mchits(self, hits):
-        """Append the position, direction and t0 columns and add t0 to time"""
-        n_hits = len(hits)
-        cal = np.empty((n_hits, 9))
-        for i in range(n_hits):
-            lookup = self._calib_by_pmt_id
-            cal[i] = lookup[hits['pmt_id'][i]]
-        dir_x = cal[:, 3]
-        dir_y = cal[:, 4]
-        dir_z = cal[:, 5]
-        du = cal[:, 7]
-        floor = cal[:, 8]
-        pos_x = cal[:, 0]
-        pos_y = cal[:, 1]
-        pos_z = cal[:, 2]
-        t0 = cal[:, 6]
-
-        hits.time += t0
-
-        return hits.append_columns([
-            'dir_x', 'dir_y', 'dir_z', 'du', 'floor', 'pos_x', 'pos_y',
-            'pos_z', 't0'
-        ], [dir_x, dir_y, dir_z, du, floor, pos_x, pos_y, pos_z, t0],
-                                   dtypes=[
-                                       '<f8', '<f8', '<f8', '<u2', '<u2',
-                                       '<f8', '<f8', '<f8', '<f8'
-                                   ])
-
     def _create_dom_channel_lookup(self):
-        data = {}
+        if HAVE_NUMBA:
+            from numba.typed import Dict
+            from numba import types
+            data = Dict.empty(
+                key_type=types.i8, value_type=types.float64[:, :]
+            )
+        else:
+            data = {}
         for pmt in self.detector.pmts:
             if pmt.dom_id not in data:
                 data[pmt.dom_id] = np.zeros((31, 9))
-            data[pmt.dom_id][pmt.channel_id] = [
+            data[pmt.dom_id][pmt.channel_id] = np.asarray([
                 pmt.pos_x, pmt.pos_y, pmt.pos_z, pmt.dir_x, pmt.dir_y,
                 pmt.dir_z, pmt.t0, pmt.du, pmt.floor
-            ]
+            ],
+                                                          dtype=np.float64)
         self._calib_by_dom_and_channel = data
         if HAVE_NUMBA:
             self._lookup_tables = [(dom, cal) for dom, cal in data.items()]
 
     def _create_pmt_id_lookup(self):
-        data = {}
+        if HAVE_NUMBA:
+            from numba.typed import Dict
+            from numba import types
+            data = Dict.empty(key_type=types.i8, value_type=types.float64[:])
+        else:
+            data = {}
         for pmt in self.detector.pmts:
-            data[pmt.pmt_id] = np.array((
+            data[pmt.pmt_id] = np.asarray([
                 pmt.pos_x,
                 pmt.pos_y,
                 pmt.pos_z,
@@ -229,7 +251,8 @@ class Calibration(Module):
                 pmt.t0,
                 pmt.du,
                 pmt.floor,
-            ))
+            ],
+                                          dtype=np.float64)
         self._calib_by_pmt_id = data
 
     def __repr__(self):
@@ -239,22 +262,62 @@ class Calibration(Module):
         return "Calibration: det_id({0})".format(self.det_id)
 
 
-if HAVE_NUMBA:
+@jit
+def apply_t0_nb(times, dom_ids, channel_ids, lookup_tables):
+    """Apply t0s using a lookup table of tuples (dom_id, calib)"""
+    dom_id = 0
+    lookup = np.empty((31, 9))
+    for i in range(len(times)):
+        cur_dom_id = dom_ids[i]
+        if cur_dom_id != dom_id:
+            dom_id = cur_dom_id
+            for (d, m) in lookup_tables:
+                if d == dom_id:
+                    np.copyto(lookup, m)
+        t0 = lookup[channel_ids[i]][6]
+        times[i] += t0
 
-    @nb.jit
-    def apply_t0_nb(times, dom_ids, channel_ids, lookup_tables):
-        """Apply t0s using a lookup table of tuples (dom_id, calib)"""
-        dom_id = 0
-        lookup = np.empty((31, 9))
-        for i in range(len(times)):
-            cur_dom_id = dom_ids[i]
-            if cur_dom_id != dom_id:
-                dom_id = cur_dom_id
-                for (d, m) in lookup_tables:
-                    if d == dom_id:
-                        np.copyto(lookup, m)
-            t0 = lookup[channel_ids[i]][6]
-            times[i] += t0
+
+@jit
+def _get_calibration_for_hits(hits, lookup):
+    """Append the position, direction and t0 columns and add t0 to time"""
+    n = len(hits)
+    cal = np.empty((n, 9))
+    for i in range(n):
+        calib = lookup[hits['dom_id'][i]][hits['channel_id'][i]]
+        cal[i] = calib
+    dir_x = cal[:, 3]
+    dir_y = cal[:, 4]
+    dir_z = cal[:, 5]
+    du = cal[:, 7]
+    floor = cal[:, 8]
+    pos_x = cal[:, 0]
+    pos_y = cal[:, 1]
+    pos_z = cal[:, 2]
+
+    t0 = cal[:, 6]
+
+    return [dir_x, dir_y, dir_z, du, floor, pos_x, pos_y, pos_z, t0]
+
+
+@jit
+def _get_calibration_for_mchits(hits, lookup):
+    """Append the position, direction and t0 columns and add t0 to time"""
+    n_hits = len(hits)
+    cal = np.empty((n_hits, 9))
+    for i in range(n_hits):
+        cal[i] = lookup[hits['pmt_id'][i]]
+    dir_x = cal[:, 3]
+    dir_y = cal[:, 4]
+    dir_z = cal[:, 5]
+    du = cal[:, 7]
+    floor = cal[:, 8]
+    pos_x = cal[:, 0]
+    pos_y = cal[:, 1]
+    pos_z = cal[:, 2]
+    t0 = cal[:, 6]
+
+    return [dir_x, dir_y, dir_z, du, floor, pos_x, pos_y, pos_z, t0]
 
 
 class CalibrationService(Module):
