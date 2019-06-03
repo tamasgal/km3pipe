91a8965f1e8358653aac076fc1b78a36749a23a7
Piotr Kalaczy?ski <pkalaczynski@km3net.de>
2019-06-03 23:22:05 +0200

    Maybe the last style fix?

--- km3pipe/io/tests/test_aanet.py
+++ km3pipe/io/tests/test_aanet.py
@@ -111,8 +111,7 @@ class TestAanetPump(TestCase):
     def test_against_single_file_freeze(self):
         start_time = time.time()
 
-        aanet_pump = AanetPump(filename = join(TEST_DATA_DIR, 'mupage.root'))
-
+        aanet_pump = AanetPump(filename=join(TEST_DATA_DIR, 'mupage.root'))
         assert 10 > (time.time() - start_time)
     
     def test_reading_hits_from_multiple_files(self):
