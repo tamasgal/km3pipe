6eda8a234decc0898e7f916322ae9978c8ff96c6
Tamas Gal <tgal@km3net.de>
2020-02-06 10:35:43 +0100

    Import submodules of km3modules into top namespace

--- km3modules/__init__.py
+++ km3modules/__init__.py
@@ -9,3 +9,14 @@ from __future__ import absolute_import, print_function, division
 
 from .common import Dump, Keep, Delete, StatusBar
 from .mc import GlobalRandomState
+
+from . import common
+from . import communication
+from . import fit
+from . import hardware
+from . import hits
+from . import k40
+from . import mc
+from . import parser
+from . import plot
+from . import topology
