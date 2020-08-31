6ab95a0f62dde2b5a7b6b09264664823e55a9daf
zineb aly <aly.zineb.az@gmail.com>
2020-08-31 14:35:11 +0200

    adapt tests to recarray

--- km3pipe/tests/test_physics.py
+++ km3pipe/tests/test_physics.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 from km3pipe.testing import TestCase, data_path
 from km3pipe.physics import get_cherenkov_photon
+from km3pipe.constants import SIN_CHERENKOV, TAN_CHERENKOV, V_LIGHT_WATER, C_LIGHT
 
 __author__ = "Zineb ALY"
 __copyright__ = "Copyright 2017, Tamas Gal and the KM3NeT collaboration."
@@ -16,60 +17,49 @@ __email__ = "zaly@km3net.de"
 __status__ = "Development"
 
 
-# Physics constants
-WATER_INDEX = 1.3499
-DN_DL = 0.0298
-COS_CHERENKOV = 1 / WATER_INDEX
-CHERENKOV_ANGLE_RAD = math.acos(COS_CHERENKOV)
-SIN_CHERENKOV = math.sin(CHERENKOV_ANGLE_RAD)
-TAN_CHERENKOV = math.tan(CHERENKOV_ANGLE_RAD)
-C_LIGHT = 299792458e-9
-V_LIGHT_WATER = C_LIGHT / (WATER_INDEX + DN_DL)
-
-
 class TestGetCherenkovPhotons(TestCase):
+    def setUp(self):
+        self.expected_t = -1 / (TAN_CHERENKOV * C_LIGHT) + 1 / (
+            SIN_CHERENKOV * V_LIGHT_WATER
+        )
+        self.track = {
+            "pos_x": 0,
+            "pos_y": 0,
+            "pos_z": 0,
+            "dir_x": 0,
+            "dir_y": 1,
+            "dir_z": 0,
+            "t": 0,
+        }
+        self.calib_hits = {
+            "pos_x": [1, 1],
+            "pos_y": [0, 0],
+            "pos_z": [0, 0],
+            "dir_x": [0, 0],
+            "dir_y": [0, 0],
+            "dir_z": [0, 0],
+        }
+
     def test_with_basic_values(self):
+        trk = pd.Series(self.track)
+        hits = pd.DataFrame(self.calib_hits)
 
-        track = pd.Series(
-            {
-                "pos_x": 0,
-                "pos_y": 0,
-                "pos_z": 0,
-                "dir_x": 0,
-                "dir_y": 1,
-                "dir_z": 0,
-                "t": 0,
-            }
-        )
-        calib_hits = pd.DataFrame(
-            {
-                "pos_x": [1, 1],
-                "pos_y": [0, 0],
-                "pos_z": [0, 0],
-                "dir_x": [0, 0],
-                "dir_y": [0, 0],
-                "dir_z": [0, 0],
-            }
-        )
+        arr = get_cherenkov_photon(hits, trk)
 
-        df = get_cherenkov_photon(calib_hits, track)
+        assert arr["d_photon_closest"][0] == 1
+        assert arr["d_photon_closest"][1] == 1
 
-        assert df["d_photon_closest"].iloc[0] == 1
-        assert df["d_photon_closest"].iloc[1] == 1
+        assert arr["d_photon"][0] == 1 / SIN_CHERENKOV
+        assert arr["d_photon"][1] == 1 / SIN_CHERENKOV
 
-        assert df["d_photon"].iloc[0] == 1 / SIN_CHERENKOV
-        assert df["d_photon"].iloc[1] == 1 / SIN_CHERENKOV
+        assert arr["d_track"][0] == -1 / TAN_CHERENKOV
+        assert arr["d_track"][1] == -1 / TAN_CHERENKOV
 
-        assert df["d_track"].iloc[0] == -1 / TAN_CHERENKOV
-        assert df["d_track"].iloc[1] == -1 / TAN_CHERENKOV
+        assert arr["t_photon"][0] == self.expected_t
+        assert arr["t_photon"][1] == self.expected_t
 
-        t = -1 / (TAN_CHERENKOV * C_LIGHT) + 1 / (SIN_CHERENKOV * V_LIGHT_WATER)
+        assert arr["cos_photon_PMT"][0] == 0
+        assert arr["cos_photon_PMT"][1] == 0
 
-        assert df["t_photon"].iloc[0] == t
-        assert df["t_photon"].iloc[1] == t
-
-        assert df["cos_photon_PMT"].iloc[0] == 0
-        assert df["cos_photon_PMT"].iloc[1] == 0
-
-        assert df["dir_z_photon"].iloc[0] == 0
-        assert df["dir_z_photon"].iloc[1] == 0
+        assert arr["dir_z_photon"][0] == 0
+        assert arr["dir_z_photon"][1] == 0
