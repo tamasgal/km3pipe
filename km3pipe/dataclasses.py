1f784d32a8cf8a98b33d3c2ba9e1422c1acc5513
Tamas Gal <tgal@km3net.de>
2020-09-03 22:59:59 +0200

    Prevent modification of scalars in original dict

--- km3pipe/dataclasses.py
+++ km3pipe/dataclasses.py
@@ -247,6 +247,7 @@ class Table(np.recarray):
     def from_dict(cls, arr_dict, dtype=None, fillna=False, **kwargs):
         """Generate a table from a dictionary of arrays.
         """
+        arr_dict = arr_dict.copy()
         # i hope order of keys == order or values
         if dtype is None:
             names = sorted(list(arr_dict.keys()))
