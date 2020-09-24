a6fbe0c494d98dce83cf1f77b710ac244f854d52
Tamas Gal <tgal@km3net.de>
2020-09-24 15:14:17 +0200

    First h5extract prototype

--- km3modules/io.py
+++ km3modules/io.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
+from collections import defaultdict
+
+import numpy as np
+
 import km3pipe as kp
+
+USR_MC_TRACKS_KEYS = [b"energy_lost_in_can", b"bx", b"by", b"ichan", b"cc"]
 
 
 class HitsTabulator(kp.Module):
@@ -8,24 +14,25 @@ class HitsTabulator(kp.Module):
 
     Parameters
     ----------
-    kind: str (default="offline")
+    kind: str
       The kind of hits to tabulate:
         "offline": the hits in an offline file
         "online": snapshot and triggered hits (will be combined)
         "mc": MC hits
-    split_hits: bool (default: True)
+    split: bool (default: True)
       Defines whether the hits should be split up into individual arrays
       in a single group (e.g. hits/dom_id, hits/channel_id) or stored
       as a single HDF5Compound array (e.g. hits).
     """
+
     def configure(self):
-        self.kind = self.get("kind", default="offline")
-        self.split_hits = self.get("split_hits", default=True)
+        self.kind = self.require("kind")
+        self.split = self.get("split", default=True)
 
     def process(self, blob):
         self.cprint(blob)
         if self.kind == "offline":
-            hits = blob['event'].hits
+            hits = blob["event"].hits
             blob["Hits"] = kp.Table(
                 {
                     "channel_id": hits.channel_id,
@@ -35,7 +42,133 @@ class HitsTabulator(kp.Module):
                     "triggered": hits.trig,
                 },
                 h5loc="/hits",
-                split_h5=True,
+                split_h5=self.split,
                 name="Hits",
             )
+
+        if self.kind == "mc":
+            mc_hits = blob["event"].mc_hits
+            blob["McHits"] = kp.Table(
+                {
+                    "a": mc_hits.a,
+                    "origin": mc_hits.origin,
+                    "pmt_id": mc_hits.pmt_id,
+                    "time": mc_hits.t,
+                },
+                h5loc="/mc_hits",
+                split_h5=self._split_hits,
+                name="McHits",
+            )
+        return blob
+
+
+class MCTracksTabulator(kp.Module):
+    """
+    Create `kp.Table` from MC tracks provided by `km3io`.
+
+    Parameters
+    ----------
+    split: bool (default: True)
+      Defines whether the tracks should be split up into individual arrays
+      in a single group (e.g. tracks/dom_id, hits/channel_id) or stored
+      as a single HDF5Compound array (e.g. hits).
+    read_usr_data: bool (default: False)
+      Parses usr-data which is originally meant for user stored values, but
+      was abused by generator software to store properties. This issue will
+      be sorted out hopefully soon as it dramatically decreases the processing
+      performance and usability.
+    """
+
+    def configure(self):
+        self.split = self.get("split", default=True)
+
+        self._read_usr_data = self.get("read_usr_data", default=False)
+        if self._read_usr_data:
+            self.log.warning(
+                "Reading usr-data will massively decrease the performance."
+            )
+
+    def process(self, blob):
+        mc_tracks = blob["event"].mc_tracks
+        blob["McTracks"] = self._parse_mc_tracks(mc_tracks)
+        return blob
+
+    def _parse_usr_to_dct(self, mc_tracks):
+        dct = defaultdict(list)
+        for k in USR_MC_TRACKS_KEYS:
+            dec_key = k.decode("utf_8")
+            for i in range(mc_tracks.usr_names.shape[0]):
+                value = np.nan
+                if k in mc_tracks.usr_names[i]:
+                    mask = mc_tracks.usr_names[i] == k
+                    value = mc_tracks.usr[i][mask][0]
+                dct[dec_key].append(value)
+        return dct
+
+    def _parse_mc_tracks(self, mc_tracks):
+        dct = {
+            "dir_x": mc_tracks.dir_x,
+            "dir_y": mc_tracks.dir_y,
+            "dir_z": mc_tracks.dir_z,
+            "pos_x": mc_tracks.pos_x,
+            "pos_y": mc_tracks.pos_y,
+            "pos_z": mc_tracks.pos_z,
+            "energy": mc_tracks.E,
+            "time": mc_tracks.t,
+            "type": mc_tracks.type,
+            "id": mc_tracks.id,
+            "length": mc_tracks.len,
+        }
+        if self._read_usr_data:
+            dct.update(self._parse_usr_to_dct(mc_tracks))
+        return kp.Table(dct, name="McTracks", h5loc="/mc_tracks", split_h5=self.split)
+
+
+class EventInfoTabulator(kp.Module):
+    """
+    Create `kp.Table` from event information provided by `km3io`.
+
+    """
+    def process(self, blob):
+        blob["EventInfo"] = self._parse_eventinfo(blob["event"])
+        return blob
+
+    def _parse_eventinfo(self, event):
+        wgt1, wgt2, wgt3, wgt4 = self._parse_wgts(event.w)
+        tab_data = {
+            "event_id": event.id,
+            "run_id": event.run_id,
+            "weight_w1": wgt1,
+            "weight_w2": wgt2,
+            "weight_w3": wgt3,
+            "weight_w4": wgt4,
+            "timestamp": event.t_sec,
+            "nanoseconds": event.t_ns,
+            "mc_time": event.mc_t,
+            "trigger_mask": event.trigger_mask,
+            "trigger_counter": event.trigger_counter,
+            "overlays": event.overlays,
+            "det_id": event.det_id,
+            "frame_index": event.frame_index,
+            "mc_run_id": event.mc_run_id,
+        }
+        info = kp.Table(tab_data, h5loc="/event_info", name="EventInfo")
+        return info
+
+    @staticmethod
+    def _parse_wgts(wgt):
+        if len(wgt) == 3:
+            wgt1, wgt2, wgt3 = wgt
+            wgt4 = np.nan
+        elif len(wgt) == 4:
+            # what the hell is w4?
+            wgt1, wgt2, wgt3, wgt4 = wgt
+        else:
+            wgt1 = wgt2 = wgt3 = wgt4 = np.nan
+        return wgt1, wgt2, wgt3, wgt4
+
+
+class OfflineHeaderTabulator(kp.Module):
+    def process(self, blob):
+        blob['RawHeader'] = kp.io.hdf5.header2table(blob['header'])
         return blob
--- km3pipe/utils/h5extract.py
+++ km3pipe/utils/h5extract.py
@@ -4,14 +4,22 @@
 A tool to extract data from KM3NeT ROOT files to HDF5.
 
 Usage:
-    h5extract [-o OUTFILE] FILENAME
+    h5extract [options] FILENAME
     h5extract (-h | --help)
     h5extract --version
 
 Options:
-    -o OUTFILE          Output file.
-    -h --help           Show this screen.
-    --version           Show the version.
+    -o OUTFILE            Output file.
+    --offline-header      Extract the offline header.
+    --event-info          Extract event information.
+    --offline-hits        Extract offline hits.
+    --mc-hits             Extract MC hits.
+    --online-hits         Extract snapshot and triggered hits (combined).
+    --mc-tracks           Extract MC tracks.
+    --mc-tracks-usr-data  Extract usr data from MC tracks (this will be slow).
+    --timeit              Print detailed pipeline performance statistics.
+    -h --help             Show this screen.
+    --version             Show the version.
 
 """
 import km3pipe as kp
@@ -22,6 +30,7 @@ log = kp.logger.get_logger(__name__)
 
 def blob_printer(blob):
     print(blob)
+    return blob
 
 
 def main():
@@ -29,16 +38,27 @@ def main():
 
     args = docopt(__doc__, version=kp.version)
 
-    print(args)
+    # print(args)
 
-    outfile = args['-o']
+    outfile = args["-o"]
     if outfile is None:
-        outfile = args['FILENAME'] + ".h5"
+        outfile = args["FILENAME"] + ".h5"
 
-    pipe = kp.Pipeline(timeit=True)
-    pipe.attach(kp.io.OfflinePump, filename=args['FILENAME'])
-    pipe.attach(km.StatusBar, every=1)
-    pipe.attach(km.io.HitsTabulator)
+    pipe = kp.Pipeline(timeit=args['--timeit'])
+    pipe.attach(kp.io.OfflinePump, filename=args["FILENAME"])
+    pipe.attach(km.StatusBar, every=1000)
+    if args["--offline-header"]:
+        pipe.attach(km.io.OfflineHeaderTabulator)
+    if args["--event-info"]:
+        pipe.attach(km.io.EventInfoTabulator)
+    if args["--offline-hits"]:
+        pipe.attach(km.io.HitsTabulator, kind="offline")
+    if args["--online-hits"]:
+        pipe.attach(km.io.HitsTabulator, kind="online")
+    if args["--mc-hits"]:
+        pipe.attach(km.io.HitsTabulator, kind="mc")
+    if args["--mc-tracks"]:
+        pipe.attach(km.io.MCTracksTabulator, read_usr_data=args["--mc-tracks-usr-data"])
     pipe.attach(blob_printer)
     pipe.attach(kp.io.HDF5Sink, filename=outfile)
     pipe.drain()
