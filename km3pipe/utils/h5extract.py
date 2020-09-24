c4c5498585dfa0f0d2c60390b16389cc2019529d
Tamas Gal <tgal@km3net.de>
2020-09-24 13:18:11 +0200

    Preliminary h5extract procedure

--- km3pipe/utils/h5extract.py
+++ km3pipe/utils/h5extract.py
@@ -1,23 +1,27 @@
 #!/usr/bin/env python3
 # Filename: h5extract.py
 """
-A tool to extract data from ROOT or EVT files to HDF5.
+A tool to extract data from KM3NeT ROOT files to HDF5.
 
 Usage:
-    h5extract filename
+    h5extract [-o OUTFILE] FILENAME
     h5extract (-h | --help)
     h5extract --version
 
 Options:
+    -o OUTFILE          Output file.
     -h --help           Show this screen.
     --version           Show the version.
 
 """
 import km3pipe as kp
+import km3modules as km
 
 log = kp.logger.get_logger(__name__)
 
 
+def blob_printer(blob):
+    print(blob)
 
 
 def main():
@@ -26,3 +30,15 @@ def main():
     args = docopt(__doc__, version=kp.version)
 
     print(args)
+
+    outfile = args['-o']
+    if outfile is None:
+        outfile = args['FILENAME'] + ".h5"
+
+    pipe = kp.Pipeline(timeit=True)
+    pipe.attach(kp.io.OfflinePump, filename=args['FILENAME'])
+    pipe.attach(km.StatusBar, every=1)
+    pipe.attach(km.io.HitsTabulator)
+    pipe.attach(blob_printer)
+    pipe.attach(kp.io.HDF5Sink, filename=outfile)
+    pipe.drain()
