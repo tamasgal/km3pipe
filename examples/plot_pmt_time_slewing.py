5e259084024ac05dd4fa40dc27ecf1846d477411
Tamas Gal <tgal@km3net.de>
2020-10-16 15:37:01 +0200

    Update time slewing example

--- examples/plot_pmt_time_slewing.py
+++ examples/plot_pmt_time_slewing.py
@@ -14,6 +14,11 @@ are corrected to refer to comparable arrival times.
 The time slewing is subtracted from the measured hit time, in contrast
 to the time calibration (t0), which is added.
 
+The time slewing correction is automatically applied in `km3pipe` when
+using `kp.calib.Calibration().apply()`, however it can be turned off
+by providing `correct_slewing=False` and also the variant can be picked
+by `slewing_variant=X`.
+
 Variant 3 is currently (as of 2020-10-16) also used in Jpp.
 
 """
@@ -27,6 +32,17 @@ import matplotlib.pyplot as plt
 kp.style.use()
 
 tots = np.arange(256)
+
+##############################
+# The `kp.cali.slew()` function can be used to calculate the slew.
+# It takes a single ToT or an array of ToTs and optionally a variant.
+# Here is the docstring of the function:
+
+help(kp.calib.slew)
+
+##############################
+# Calculating the slew for all variants:
+
 slews = {variant: kp.calib.slew(tots, variant=variant) for variant in (1, 2, 3)}
 
 fig, ax = plt.subplots()
