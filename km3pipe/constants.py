1d70b6fe1c462dc6bc9b953c117a0ccfa990924f
Tamas Gal <tgal@km3net.de>
2020-10-20 23:47:28 +0200

    Clean up constants

--- km3pipe/constants.py
+++ km3pipe/constants.py
@@ -16,8 +16,8 @@ __email__ = "tgal@km3net.de"
 __status__ = "Development"
 
 # Physics constants used in physics module
-WATER_INDEX = 1.3499
-INDEX_OF_REFRACTION_WATER = 1.3800851282
+WATER_INDEX = 1.3499  # Used in aanet
+INDEX_OF_REFRACTION_WATER = 1.3800851282  # Used in Jpp (e.g. in PDFs)
 DN_DL = 0.0298
 COS_CHERENKOV = 1 / WATER_INDEX
 CHERENKOV_ANGLE_RAD = math.acos(COS_CHERENKOV)
@@ -27,25 +27,4 @@ C_LIGHT = 299792458e-9
 V_LIGHT_WATER = C_LIGHT / (WATER_INDEX + DN_DL)
 C_WATER = C_LIGHT / INDEX_OF_REFRACTION_WATER
 
-# Detector related parameters
-arca_frame_duration = 0.1  # s
-orca_frame_duration = 0.1  # s
-
 c = 2.99792458e8  # m/s
-
-n_water_antares_phase = 1.3499
-n_water_antares_group = 1.3797
-n_water_km3net_group = 1.3787
-n_water_antares = n_water_antares_group
-theta_cherenkov_water_antares = math.acos(1 / n_water_antares_phase)
-theta_cherenkov_water_km3net = math.acos(1 / n_water_km3net_group)
-c_water_antares = c / n_water_antares_group
-c_water_km3net = c / n_water_km3net_group
-
-# Math
-pi = math.pi
-e = math.e
-
-# Default values for time residuals
-dt_window_l = -15  # ns
-dt_window_h = +25  # ns
