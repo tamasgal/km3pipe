e9d332fc311f5f3e2457ce2fcfa3e197b82a29b4
Tamas Gal <tgal@km3net.de>
2020-10-20 17:45:17 +0200

    Improve sphere cut example

--- examples/plot_spherecuts.py
+++ examples/plot_spherecuts.py
@@ -15,27 +15,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-################
+#######################
 # Generating a few points randomly distributed in a cube
-points = np.random.rand(2000, 3)
 
-fig = plt.figure()
-ax = fig.gca(projection="3d")
+points = np.random.rand(1000, 3)
 
-################
-# This will select all points which are inside a sphere with radius 0.5
-# and centered at (0.5, 0.5, 0.5):
+#######################
+# In this example we create two masks and use them to not only highlight the
+# selected points but also to hide those from the original dataset.
 
-selected_points = kp.math.spherecut([0.5, 0.5, 0.5], 0.0, 0.5, points)
-
-ax.scatter(*selected_points.T)
-
-################
-# Sometimes it's better to use (boolean) masks which can later be used
-# to combine multiple selections. In this example we create two
-# masks and use them to not only highlight the selected points but
-# also to hide those from the original dataset.
-#
 points = np.random.rand(1000, 3)
 
 mask1 = kp.math.spherecutmask([0, 0, 0], 0.8, 1.0, points)
@@ -47,7 +35,18 @@ ax.scatter(*points[~(mask1 | mask2)].T, label="unselected points")
 ax.scatter(*points[mask1].T, label="mask1")
 ax.scatter(*points[mask2].T, label="mask2")
 
-ax.legend()
+ax.legend();
+
+#######################
+# This will select all points which are inside a sphere with radius 0.5
+# and centered at (0.5, 0.5, 0.5):
+
+selected_points = kp.math.spherecut([0.5, 0.5, 0.5], 0.0, 0.5, points)
+
+fig = plt.figure()
+ax = fig.gca(projection="3d")
+
+ax.scatter(*selected_points.T)
 
 fig.tight_layout()
 plt.show()
