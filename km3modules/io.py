28d27f0dfb28f8030fb8dd7f01e0bb00178ff42d
Daniel Guderian <dguderian@km3net.de>
2021-05-17 06:28:24 +0000

    Resolve "Adjust h5extract and km3modules' io"

--- km3modules/io.py
+++ km3modules/io.py
@@ -307,10 +307,20 @@ class EventInfoTabulator(kp.Module):
     """
 
     def process(self, blob):
-        blob["EventInfo"] = self._parse_eventinfo(blob["event"])
+
+        # get the sim program
+        if blob["header"]:
+            if "simul" in blob["header"].keys():
+                sim_program = blob["header"].simul.program
+            else:  # not existent for real data
+                sim_program = None
+        else:
+            sim_program = None
+
+        blob["EventInfo"] = self._parse_eventinfo(blob["event"], sim_program)
         return blob
 
-    def _parse_eventinfo(self, event):
+    def _parse_eventinfo(self, event, sim_program):
         wgt1, wgt2, wgt3, wgt4 = self._parse_wgts(event.w)
         tab_data = {
             "event_id": event.id,
@@ -329,8 +339,31 @@ class EventInfoTabulator(kp.Module):
             "frame_index": event.frame_index,
             "mc_run_id": event.mc_run_id,
         }
+
+        if sim_program != None:
+
+            # unfold the info in the w2list
+            w2list_dict = self._unfold_w2list(event.w2list, sim_program)
+            tab_data.update(w2list_dict)
+
         info = kp.Table(tab_data, h5loc="/event_info", name="EventInfo")
         return info
+
+    def _unfold_w2list(self, w2list, sim_program):
+
+        w2list_dict = {}
+        definitions_dict = {}
+
+        if sim_program.lower() == "gseagen":
+            definitions_dict = km3io.definitions.w2list_gseagen
+        elif sim_program.lower() == "genhen":
+            definitions_dict = km3io.definitions.w2list_genhen
+        # for cases like sim_program == "MUPAGE", the w2list is empty
+
+        for key, idx in definitions_dict.items():
+            w2list_dict[key] = np.nan if idx >= len(w2list) else w2list[idx]
+
+        return w2list_dict
 
     @staticmethod
     def _parse_wgts(wgt):
