688a64cee298e3adea89d73968ad69dde60d5f0d
Tamas Gal <himself@tamasgal.com>
2021-09-28 00:06:58 +0200

    Add command line tool tests

---
+++ benchmarks/cli/command_line_tools.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+km3pipe --help
+pipeinspector --help
+h5extract --help
+h5extractf --help
+h5info --help
+h5tree --help
+h5header --help
+ztplot --help
+k40calib --help
+triggermap --help
+nb2sphx --help
+ligiermirror --help
+qrunprocessor --help
+qrunqaqc --help
+daqsample --help
+tres --help
